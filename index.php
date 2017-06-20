81d209143017dc1def6a00e0a113a4622fc83fa2
Fábio Valentim <maitabom@gmail.com>
2017-06-20 14:26:36 -0300

    Configuração inicial do projeto

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 
-require_once 'vendor/autoload.php';
+require 'config.php';
+
 
 $loader = new Twig_Loader_Array(array(
     'index' => 'Hello {{ name }}!',
 ));
 $twig = new Twig_Environment($loader);
 
-echo $twig->render('index', array('name' => 'Fabien'));
+echo $twig->render('index', array('name' => 'Prefeito'));
