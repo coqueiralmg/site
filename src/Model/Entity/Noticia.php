335350bea74f88a2306b8b01c1fe6ac8ac011f97
Fábio Valentim <maitabom@gmail.com>
2017-06-21 11:11:49 -0300

    Criação de propriedade personalizadas para manipulação de notícias

--- src/Model/Entity/Noticia.php
+++ src/Model/Entity/Noticia.php
@@ -7,5 +7,28 @@ use Cake\ORM\Entity;
 
 class Noticia extends Entity
 {
-    
+    protected function _getResumo()
+    {
+        $limiteCaracteres = 250;
+        $texto = $this->_properties['texto'];
+        $texto = strip_tags($texto);
+        $reticences = "";
+
+        if (strlen($texto) > $limiteCaracteres){
+            $reticences = "...";
+            $limite = substr($texto, 0, $limiteCaracteres);
+            $posicaoString = strrpos($limite, " ");
+            $cortaTexto = ($posicaoString > 0) ? $posicaoString : strlen($limite);
+            $texto = substr($limite, 0, $cortaTexto);
+        }
+
+        return $texto . $reticences;
+    }
+
+    protected function _getRaw()
+    {
+        $texto = $this->_properties['texto'];
+
+        return strip_tags($texto);
+    }
 }
