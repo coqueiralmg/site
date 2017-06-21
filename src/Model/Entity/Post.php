1be2215de6cf76110a82793a25691c96f0d1c447
Fábio Valentim <maitabom@gmail.com>
2017-06-21 11:14:00 -0300

    Ajustes na entidade de postagem

--- src/Model/Entity/Post.php
+++ src/Model/Entity/Post.php
@@ -3,6 +3,7 @@
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
+use Cake\Utility\Text;
 
 
 class Post extends Entity
@@ -12,7 +13,7 @@ class Post extends Entity
         $titulo = $this->_properties['titulo'];
         $procurar = array(' ', 'ã', 'à', 'á', 'â', 'é', 'ê', 'í', 'ì', 'ó', 'ò', 'õ', 'ô', 'ú', 'ù', 'û', 'ç', ',', '.', '!', '?', ';', '/');
         $substituir = array('_', 'a', 'a', 'a', 'a', 'e', 'e', 'i', 'i', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'c', '', '', '', '', '', '_');
-
+        
         return strtolower(str_replace($procurar, $substituir, $titulo));
     }
 }
