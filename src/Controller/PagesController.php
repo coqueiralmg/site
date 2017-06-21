e5df4d281f6a281c605ad2321df4ab4e12e0fac8
Fábio Valentim <maitabom@gmail.com>
2017-06-21 10:05:39 -0300

    Configuração do controller para página inicial

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -15,9 +15,8 @@
 namespace App\Controller;
 
 use Cake\Core\Configure;
-use Cake\Network\Exception\ForbiddenException;
-use Cake\Network\Exception\NotFoundException;
-use Cake\View\Exception\MissingTemplateException;
+use Cake\ORM\TableRegistry;
+use Cake\ORM\Entity;
 
 /**
  * Static content controller
@@ -27,43 +26,18 @@ use Cake\View\Exception\MissingTemplateException;
  * @link http://book.cakephp.org/3.0/en/controllers/pages-controller.html
  */
 class PagesController extends AppController
-{
+{   
 
-    /**
-     * Displays a view
-     *
-     * @param string ...$path Path segments.
-     * @return void|\Cake\Network\Response
-     * @throws \Cake\Network\Exception\ForbiddenException When a directory traversal attempt.
-     * @throws \Cake\Network\Exception\NotFoundException When the view file could not
-     *   be found or \Cake\View\Exception\MissingTemplateException in debug mode.
-     */
-    public function display(...$path)
+    public function home()
     {
-        $count = count($path);
-        if (!$count) {
-            return $this->redirect('/');
-        }
-        if (in_array('..', $path, true) || in_array('.', $path, true)) {
-            throw new ForbiddenException();
-        }
-        $page = $subpage = null;
+        $t_noticia = TableRegistry::get('Noticia');
+        
+        $noticias = $t_noticia->find('all', [
+            'contain' => ['Post' => ['Usuario' => ['Pessoa']]],
+            'order' => ['Post.datapostagem' => 'DESC'], 
+            'limit' => 3
+        ]);
 
-        if (!empty($path[0])) {
-            $page = $path[0];
-        }
-        if (!empty($path[1])) {
-            $subpage = $path[1];
-        }
-        $this->set(compact('page', 'subpage'));
-
-        try {
-            $this->render(implode('/', $path));
-        } catch (MissingTemplateException $e) {
-            if (Configure::read('debug')) {
-                throw $e;
-            }
-            throw new NotFoundException();
-        }
+         $this->set('noticias', $noticias);
     }
 }
