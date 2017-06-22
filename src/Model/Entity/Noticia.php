c3b498782555f3b595a235949f30a1f16db80188
Fábio Valentim <maitabom@gmail.com>
2017-06-22 15:42:46 -0300

    Criação da propriedade para resumo longo da notícia

--- src/Model/Entity/Noticia.php
+++ src/Model/Entity/Noticia.php
@@ -25,6 +25,24 @@ class Noticia extends Entity
         return $texto . $reticences;
     }
 
+    protected function _getParte()
+    {
+        $limiteCaracteres = 500;
+        $texto = $this->_properties['texto'];
+        $texto = strip_tags($texto);
+        $reticences = "";
+
+        if (strlen($texto) > $limiteCaracteres){
+            $reticences = "...";
+            $limite = substr($texto, 0, $limiteCaracteres);
+            $posicaoString = strrpos($limite, " ");
+            $cortaTexto = ($posicaoString > 0) ? $posicaoString : strlen($limite);
+            $texto = substr($limite, 0, $cortaTexto);
+        }
+
+        return $texto . $reticences;
+    }
+
     protected function _getRaw()
     {
         $texto = $this->_properties['texto'];
