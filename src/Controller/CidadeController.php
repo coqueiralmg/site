04ea8764703743df540dee5d0776a552b8ee5b79
Fábio Valentim <maitabom@gmail.com>
2017-06-22 09:22:45 -0300

    Adição de recurso de tags sociais

--- src/Controller/CidadeController.php
+++ src/Controller/CidadeController.php
@@ -9,7 +9,21 @@ class CidadeController extends AppController
 {
     public function historico()
     {
+       $socialTags = [
+           'og:locale' => 'pt_BR',
+           'og:type' => 'article',
+           'article:publisher' => 'https://www.facebook.com/prefeituradecoqueiral',
+           'og:title' => 'História de Coqueiral | Prefeitura Municipal de Coqueiral',
+           'og:description' => 'A história da cidade de Coqueiral - MG.',
+           'og:url' => 'http://coqueiral.mg.gov.br/cidade/historico',
+           'og:site_name' => 'Prefeitura Municipal de Coqueiral',
+           'og:image' => 'img/slide/slider_historico1.jpg',
+           'og:image:width' => '600',
+           'og:image:height' => '400'
+       ];
+       
        $this->set('title', "História de Coqueiral");
+       $this->set('socialTags', $socialTags);
     }
 
     public function perfil()
