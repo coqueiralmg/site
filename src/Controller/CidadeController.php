8f697953f1c1f12ee9be0f2c84aea718b3cd777b
Fábio Valentim <maitabom@gmail.com>
2017-06-22 09:29:18 -0300

    Adição de tags social para perfil da cidade

--- src/Controller/CidadeController.php
+++ src/Controller/CidadeController.php
@@ -17,7 +17,7 @@ class CidadeController extends AppController
            'og:description' => 'A história da cidade de Coqueiral - MG.',
            'og:url' => 'http://coqueiral.mg.gov.br/cidade/historico',
            'og:site_name' => 'Prefeitura Municipal de Coqueiral',
-           'og:image' => 'img/slide/slider_historico1.jpg',
+           'og:image' => '../img/slide/slider_historico1.jpg',
            'og:image:width' => '600',
            'og:image:height' => '400'
        ];
@@ -28,7 +28,21 @@ class CidadeController extends AppController
 
     public function perfil()
     {
+         $socialTags = [
+           'og:locale' => 'pt_BR',
+           'og:type' => 'article',
+           'article:publisher' => 'https://www.facebook.com/prefeituradecoqueiral',
+           'og:title' => 'Perfil do Município de Coqueiral | Prefeitura Municipal de Coqueiral',
+           'og:description' => 'Perfil e dados gerais sobre a cidade de Coqueiral - MG.',
+           'og:url' => 'http://coqueiral.mg.gov.br/cidade/historico',
+           'og:site_name' => 'Prefeitura Municipal de Coqueiral',
+           'og:image' => '../img/slide/praca.jpg',
+           'og:image:width' => '600',
+           'og:image:height' => '400'
+       ];
+        
         $this->set('title', "O Perfil do Município");
+        $this->set('socialTags', $socialTags);
     }
 
     public function localizacao()
