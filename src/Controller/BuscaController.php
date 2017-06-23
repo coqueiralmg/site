4af9c4efb7767263f5f8725d5d000f22e2408afc
Fábio Valentim <maitabom@gmail.com>
2017-06-23 14:03:43 -0300

    Criação de controller de busca

---
+++ src/Controller/BuscaController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Cake\Core\Configure;
+use Cake\ORM\TableRegistry;
+
+class BuscaController extends AppController
+{
+    public function index()
+    {
+
+    }
+}
