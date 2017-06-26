99d285896bb7ae8f137175d16700d68170a5a7e7
Fábio Valentim <maitabom@gmail.com>
2017-06-26 10:05:50 -0300

    Criação do controller principal do sistema

---
+++ admin/src/Controller/SystemController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Cake\Core\Configure;
+use Cake\Network\Session;
+use Cake\ORM\TableRegistry;
+
+
+class SystemController extends AppController
+{
+
+    public function initialize()
+    {
+        parent::initialize();
+    }
+
+    public function login()
+    {
+        $this->viewBuilder()->layout('guest');
+        $this->set('title', "Controle de Acesso");
+    }
+
+    public function board()
+    {
+
+    }
+}
