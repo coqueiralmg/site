c68af5d52ae9b16800468ab4698c28ed6a086f96
Fábio Valentim <maitabom@gmail.com>
2017-07-06 07:29:49 -0300

    Boas práticas JavaScript

--- admin/webroot/js/painel.js
+++ admin/webroot/js/painel.js
@@ -3,13 +3,13 @@ $(document).ready(function () {
 });
 
 function obterDataHora() {
-    var momentoAtual = new Date()
+    var momentoAtual = new Date();
     var dia = momentoAtual.getDate();
     var mes = momentoAtual.getMonth() + 1;
     var ano = momentoAtual.getFullYear();
-    var hora = momentoAtual.getHours()
-    var minuto = momentoAtual.getMinutes()
-    var segundo = momentoAtual.getSeconds()
+    var hora = momentoAtual.getHours();
+    var minuto = momentoAtual.getMinutes();
+    var segundo = momentoAtual.getSeconds();
 
     var horaImprimivel = ((dia < 10) ? "0" + dia : dia) + "/" + ((mes < 10) ? "0" + mes : mes) + "/" + ano + " " + ((hora < 10) ? "0" + hora : hora) + ":" + ((minuto < 10) ? "0" + minuto : minuto) + ":" + ((segundo < 10) ? "0" + segundo : segundo);
 
@@ -21,5 +21,5 @@ function imprimeRelogio() {
 
     $("#hora_atual").html(atual);
 
-    setTimeout("imprimeRelogio()", 1000)
+    setTimeout("imprimeRelogio()", 1000);
 }
