8f9523547775a55f75f623c3385d6b7c3e591367
Fábio Valentim <maitabom@gmail.com>
2017-07-06 15:51:52 -0300

    Criação de actions de cadastro

--- admin/src/Controller/UsuariosController.php
+++ admin/src/Controller/UsuariosController.php
@@ -17,9 +17,26 @@ class UsuariosController extends AppController
 
     public function index()
     {
-        $this->set('title', "Lista de Usuários");
+        $this->set('title', 'Lista de Usuários');
         $this->set('icon', 'person');
     }
 
-    
+    public function add()
+    {
+        $this->redirect(['action' => 'cadastro', 0]);
+    }
+
+    public function edit(int $id)
+    {
+        $this->redirect(['action' => 'cadastro', $id]);
+    }
+
+    public function cadastro(int $id)
+    {
+        $title = ($id > 0) ? 'Edição de Usuário' : 'Novo Usuário';
+        $icon = ($id > 0) ? 'person_outline' : 'person_add';
+
+        $this->set('title', $title);
+        $this->set('icon', $icon);
+    }
 }
