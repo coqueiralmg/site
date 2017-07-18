1fbf89fd0bf040c3f749c2df906725eff4ffe424
Fábio Valentim <maitabom@gmail.com>
2017-07-18 14:51:20 -0300

    Adição da função de bloquear o IP

--- admin/src/Controller/Component/FirewallComponent.php
+++ admin/src/Controller/Component/FirewallComponent.php
@@ -11,5 +11,27 @@ use Cake\ORM\TableRegistry;
  */
 class FirewallComponent extends Component
 {
-    
+    /**
+     * Bloqueia um determinado endereço de IP do sistema
+     *
+     * @param string $motivo Motivo do bloqueio ter sido ocorrido.
+     * @return int|mixed Código do registro de bloqueio, se salvo com sucesso.
+     */
+    public function bloquear(string $motivo = 'Não definido')
+    {
+        $id = 0;
+        $table = TableRegistry::get('Bloqueado');
+        $bloqueado = $table->newEntity();
+
+        $bloqueado->ip = $_SERVER['REMOTE_ADDR'];
+        $bloqueado->data_bloqueio = date("Y-m-d H:i:s");
+        $bloqueado->motivo = $motivo;
+
+        if($table->save())
+        {
+            $id = $bloqueado->id;
+        }
+
+        return $id;
+    }
 }
