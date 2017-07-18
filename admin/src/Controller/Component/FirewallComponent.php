96dffd3b92bb9521166d0017a9a5c45f90aa82e0
Fábio Valentim <maitabom@gmail.com>
2017-07-18 08:44:03 -0300

    Criação do componente de Firewall

---
+++ admin/src/Controller/Component/FirewallComponent.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller\Component;
+
+use Cake\Controller\Component;
+use Cake\ORM\TableRegistry;
+
+/**
+ * Classe que representa o componente de acesso e bloqueio ao sistema, a partir de uma determinada máquina (Firewall).
+ * @package App\Controller\Component
+ */
+class FirewallComponent extends Component
+{
+    
+}
