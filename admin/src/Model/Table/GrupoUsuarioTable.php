3ddbe37844b00781bca8861c49857a846464482a
Fábio Valentim <maitabom@gmail.com>
2017-07-18 11:04:02 -0300

    Criação de entidade e tabela para grupo de usuário

---
+++ admin/src/Model/Table/GrupoUsuarioTable.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Model\Table;
+
+class GrupoUsuarioTable extends BaseTable
+{
+    public function initialize(array $config)
+    {
+        $this->table('grupos');
+        $this->primaryKey('id');
+        /* $this->belongsToMany('Funcao', [
+            'joinTable' => 'funcoes_grupos',
+            'foreignKey' => 'grupos_id',
+            'targetForeignKey' => 'funcoes_id',
+            'propertyName' => 'Funcoes'
+        ]); */
+    }
+}
