614c4db1b208cffff30744626e324f5f046cf1af
Fábio Valentim <maitabom@gmail.com>
2017-07-24 15:54:01 -0300

    Criação do controller de logs

---
+++ admin/src/Controller/LogController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Cake\Core\Configure;
+use Cake\Network\Session;
+use Cake\ORM\TableRegistry;
+
+
+class LogController extends AppController
+{
+
+    public function initialize()
+    {
+        parent::initialize();
+    }
+
+    public function index()
+    {
+        $t_auditoria = TableRegistry::get('Auditoria');
+
+        $query = $t_auditoria->find('all', [
+            'conditions' => [
+                'usuario' =>  $this->request->session()->read('UsuarioID'),
+                'ocorrencia' => 1
+            ]
+        ]);
+        
+        $this->set('title', 'Log de Acesso');
+        $this->set('icon', 'receipt');
+        $this->set('log', $query);
+
+    }
+
+}
