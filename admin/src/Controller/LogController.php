26af2e3d0304e79a57155c41d3d9fd7bdd631789
Fábio Valentim <maitabom@gmail.com>
2017-07-25 10:40:34 -0300

    Implementação da função de imprimir log de acesso

--- admin/src/Controller/LogController.php
+++ admin/src/Controller/LogController.php
@@ -18,19 +18,53 @@ class LogController extends AppController
     public function index()
     {
         $t_auditoria = TableRegistry::get('Auditoria');
+        $limite_paginacao = Configure::read('limitPagination');
 
-        $query = $t_auditoria->find('all', [
-            'conditions' => [
-                'usuario' =>  $this->request->session()->read('UsuarioID'),
-                'ocorrencia' => 1
-            ],
-            'order' => ['data' => 'DESC']
-        ]);
+        $conditions = [
+            'usuario' =>  $this->request->session()->read('UsuarioID'),
+            'ocorrencia' => 1
+        ];
+
+        $this->paginate = [
+            'limit' => $limite_paginacao,
+            'conditions' => $conditions,
+            'order' => [
+                'data' => 'DESC'
+            ]
+        ];
+
+        $log = $this->paginate($t_auditoria);
+        $quantidade = $t_auditoria->find('all', ['conditions' => $conditions])->count();
         
         $this->set('title', 'Log de Acesso');
         $this->set('icon', 'receipt');
-        $this->set('log', $query);
-
+        $this->set('log', $log);
+        $this->set('qtd_total', $quantidade);
+        $this->set('limit_pagination', $limite_paginacao);
     }
 
+    public function imprimir()
+    {
+        $t_auditoria = TableRegistry::get('Auditoria');
+
+        $conditions = [
+            'usuario' =>  $this->request->session()->read('UsuarioID'),
+            'ocorrencia' => 1
+        ];
+
+        $log = $t_auditoria->find('all', [
+            'conditions' => $conditions,
+            'order' => [
+                'data' => 'DESC'
+            ]
+        ]);
+
+        $quantidade = $t_auditoria->find('all', ['conditions' => $conditions])->count();
+
+         $this->viewBuilder()->layout('print');
+
+        $this->set('title', 'Log de Acesso');
+        $this->set('log', $log);
+        $this->set('qtd_total', $quantidade);
+    }
 }
--- admin/src/Template/Log/index.ctp
+++ admin/src/Template/Log/index.ctp
@@ -3,6 +3,13 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="card">
+                    <div class="card-content">
+                        <a href="<?= $this->Url->build(['controller' => 'Log', 'action' => 'imprimir']) ?>" target="_blank" class="btn btn-fill btn-default pull-right">Imprimir<div class="ripple-container"></div></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="card">
                     <div class="card-content table-responsive">
                         <h4 class="card-title">Log de Acesso ao Sistema</h4>
                         <table class="table">
@@ -26,12 +33,7 @@
                      <div class="card-content">
                         <div class="material-datatables">
                             <div class="row">
-                                <div class="col-sm-5">
-                                    <div class="dataTables_paginate paging_full_numbers" id="datatables_info">5 itens encontrados</div>
-                                </div>
-                                <div class="col-sm-7 text-right">
-                                    
-                                </div>
+                               <?=$this->element('pagination') ?>
                             </div>
                         </div>
                     </div>
