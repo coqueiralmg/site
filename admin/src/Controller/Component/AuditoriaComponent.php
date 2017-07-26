4dfe79e39344a2820ff018884329038390e5b656
Fábio Valentim <maitabom@gmail.com>
2017-07-26 13:36:51 -0300

    Alteração do formato de auditoria

--- admin/src/Controller/Component/AuditoriaComponent.php
+++ admin/src/Controller/Component/AuditoriaComponent.php
@@ -12,6 +12,24 @@ use Cake\ORM\TableRegistry;
 class AuditoriaComponent extends Component
 {
     /**
+    * Coletânea de tipos de ocorrências de auditoria.
+    */
+    private $ocorrencias = [
+        1 => 'Logon no sistema',
+        2 => 'Troca de senha',
+        3 => 'Bloqueio de IP',
+        4 => 'Suspensão da conta',
+        5 => 'Limpeza de cache e sessão',
+        6 => 'Acesso suspeito',
+        7 => 'Acesso bloqueado',
+        8 => 'Logoff no sistema',
+        9 => 'Impressão de documento',
+        10 => 'Inclusão do usuário',
+        11 => 'Alteração do usuário',
+        12 => 'Exclusão do usuário'
+    ];
+    
+    /**
      * Faz o registro de auditoria no sistema, por meio de sistema.
      *
      * @param array $dados Dados a serem adicionados no banco de dados de auditoria.
@@ -82,17 +100,6 @@ class AuditoriaComponent extends Component
     */
     public function buscarNomeOcorrencia(int $codigo)
     {
-        $ocorrencias = [
-            1 => 'Logon no sistema',
-            2 => 'Troca de senha',
-            3 => 'Bloqueio de IP',
-            4 => 'Suspensão da conta',
-            5 => 'Limpeza de cache e sessão',
-            6 => 'Acesso suspeito',
-            7 => 'Acesso bloqueado',
-            8 => 'Logoff no sistema'
-        ];
-
-        return $ocorrencias[$codigo];
+        return $this->ocorrencias[$codigo];
     }
 }
