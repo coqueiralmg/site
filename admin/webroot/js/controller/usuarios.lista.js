ce27d75703dbdf4b73999daf330d0540201cd24e
Fábio Valentim <maitabom@gmail.com>
2017-07-27 10:50:03 -0300

    Criação do ClientController para listagem de usuários

---
+++ admin/webroot/js/controller/usuarios.lista.js
@@ -0,0 +1,23 @@
+function excluirUsuario(id, nome) {
+    var usuario = 0;
+
+    if (id == usuario) {
+        swal('Erro na exclusão', 'Você não pode excluir a si mesmo no sistema!', 'error');
+    } else {
+        swal({
+            title: "Deseja excluir este usuário?",
+            html: "A exclusão do usuário <b> " + nome + "</b> irá tornar a operação irreversível.",
+            input: 'checkbox',
+            inputValue: 0,
+            inputPlaceholder: ' Exclua também a trilha de auditoria.',
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonClass: 'btn btn-success',
+            cancelButtonClass: 'btn btn-danger',
+            confirmButtonText: 'Sim',
+            cancelButtonText: 'Não'
+        }).then(function (result) {
+            window.location = '/admin/usuarios/delete/' + id + '?auditoria=' + result;
+        });
+    }
+}
