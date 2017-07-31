5383aff5c393f7e1015706c4925d42a6a04f931b
Fábio Valentim <maitabom@gmail.com>
2017-07-31 09:12:19 -0300

    Adição de relacionamento na tabela de função

--- admin/src/Model/Table/FuncaoTable.php
+++ admin/src/Model/Table/FuncaoTable.php
@@ -10,5 +10,12 @@ class FuncaoTable extends BaseTable
         $this->table('funcoes');
         $this->primaryKey('id');
         $this->entityClass('Funcao');
+
+        $this->belongsToMany('GrupoUsuario', [
+            'joinTable' => 'funcoes_grupos',
+            'foreignKey' => 'funcoes_id',
+            'targetForeignKey' => 'grupos_id',
+            'propertyName' => 'grupos'
+        ]);
     }
 }
