a4458a5713847910feee4b1ee200109fce671ca0
Fábio Valentim <maitabom@gmail.com>
2017-08-01 11:18:47 -0300

    Implementação do cadastro de registro de firewall

--- admin/src/Controller/FirewallController.php
+++ admin/src/Controller/FirewallController.php
@@ -5,10 +5,10 @@ namespace App\Controller;
 use Cake\Core\Configure;
 use Cake\Network\Session;
 use Cake\ORM\TableRegistry;
+use \Exception;
 
 class FirewallController extends AppController
 {
-
     public function initialize()
     {
         parent::initialize();
@@ -79,5 +79,46 @@ class FirewallController extends AppController
         $this->set('title', 'Firewall');
         $this->set('firewall', $firewall);
         $this->set('qtd_total', $qtd_total);
+    }
+
+    public function add()
+    {
+        $this->redirect(['action' => 'cadastro', 0]);
+    }
+
+    public function edit(int $id)
+    {
+        $this->redirect(['action' => 'cadastro', $id]);
+    }
+
+    public function cadastro(int $id)
+    {
+        $title = ($id > 0) ? 'Editar Registro do Firewall' : 'Novo Registro do Firewall';
+        $icon = 'security';
+
+        $t_firewall = TableRegistry::get('Firewall');
+
+        $tipo_lista = [
+            ['value' => 'N', 'text' => 'Lista Negra', 'style' => 'margin-left:5px;'],
+            ['value' => 'B', 'text' => 'Lista Branca', 'style' => 'margin-left:15px;']
+        ];
+
+        if($id > 0)
+        {
+            $firewall = $t_firewall->get($id);
+
+            $firewall->data = $this->Format->formatDateView($firewall->data);
+
+            $this->set('firewall', $firewall);
+        }
+        else
+        {
+            $this->set('firewall', null);
+        }
+
+        $this->set('title', $title);
+        $this->set('icon', $icon);
+        $this->set('id', $id);
+        $this->set('tipo_lista', $tipo_lista);
     }
 }
