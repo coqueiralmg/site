93d5dd3c3c362404c46479d190115947829b8e76
Fábio Valentim <maitabom@gmail.com>
2017-08-04 11:24:56 -0300

    Implementação da validação na busca de publicações

--- admin/webroot/js/controller/publicacoes.lista.js
+++ admin/webroot/js/controller/publicacoes.lista.js
@@ -12,5 +12,45 @@ $(function () {
 });
 
 function validar() {
+    var mensagem = "";
+    var dataInicial = $('#data_inicial').val();
+    var dataFinal = $('#data_final').val();
 
+    if (dataInicial != "" || dataFinal != "") {
+        if (dataInicial == "") {
+            mensagem += "Favor, informe a data inicial para efetuar a busca por data.";
+            $("label[for='data-inicial']").css("color", "red");
+        } else {
+            $("label[for='data-inicial']").css("color", "#aaa");
+        }
+
+        if (dataFinal == "") {
+            mensagem += "Favor, informe a data final para efetuar a busca por data.";
+            $("label[for='data-final']").css("color", "red");
+        } else {
+            $("label[for='data-final']").css("color", "#aaa");
+        }
+
+        if (dataInicial != "" && dataFinal != "") {
+            var inicial = new Date(dataInicial);
+            var final = new Date(dataFinal);
+
+            if (inicial > final) {
+                mensagem += "A data inicial é maior do que a data final.";
+                $("label[for='data-inicial']").css("color", "red");
+                $("label[for='data-final']").css("color", "red");
+            } else {
+                $("label[for='data-inicial']").css("color", "#aaa");
+                $("label[for='data-final']").css("color", "#aaa");
+            }
+        }
+    }
+
+    if (mensagem == "") {
+        return true;
+    } else {
+        $("#lista_erro").show('shake');
+        $("#details").html("<ol>" + mensagem + "</ol>");
+        return false;
+    }
 }
