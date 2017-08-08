8a4600adfa1bdb441553bfa92c5268a1b4f35f30
Fábio Valentim <maitabom@gmail.com>
2017-08-08 10:53:34 -0300

    Criação do componente de gerenciamento de arquivos

---
+++ admin/src/Controller/Component/FileComponent.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller\Component;
+
+use Cake\Controller\Component;
+
+/**
+ * Classe que representa o componente de controle e gerenciamento de arquivos dentro do sistema.
+ * @package App\Controller\Component
+ */
+class FileComponent extends Component
+{
+
+}
