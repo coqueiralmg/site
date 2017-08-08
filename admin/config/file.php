87f4f59d4d038b77e5f82db326587cae62e540ae
Fábio Valentim <maitabom@gmail.com>
2017-08-08 13:07:33 -0300

    Definição de configurações de arquivos

--- admin/config/file.php
+++ admin/config/file.php
@@ -1,7 +1,16 @@
 <?php
 
-return[
+return [
     'Files' => [
-        'validation'
+        'validation' => [
+            'document' => [
+                'types' => ['pdf', 'doc', 'docx', 'odt', 'xls', 'xlsx', 'ods', 'zip'],
+                'maxLength' => 102400
+            ],
+            'image' => [
+                'types' => ['gif', 'jpg', 'jpeg', 'png'],
+                'maxLength' => 5120
+            ]
+        ]
     ]
 ];
