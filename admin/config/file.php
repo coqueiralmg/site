4d4a421d92be735976fe2488b834d95001429b7d
Fábio Valentim <maitabom@gmail.com>
2017-08-10 10:22:54 -0300

    Configurações de tamanho, dados fixos e locais de upload de arquivos

--- admin/config/file.php
+++ admin/config/file.php
@@ -5,12 +5,24 @@ return [
         'validation' => [
             'document' => [
                 'types' => ['pdf', 'doc', 'docx', 'odt', 'xls', 'xlsx', 'ods', 'zip'],
-                'maxLength' => 102400
+                'maxLength' => 31457280
             ],
             'image' => [
                 'types' => ['gif', 'jpg', 'jpeg', 'png'],
-                'maxLength' => 5120
+                'maxLength' => 5242880
             ]
+        ],
+        'misc' => [
+            'kbyte' => 1024,
+            'megabyte' => 1048576,
+            'gigabyte' => 1073741824,
+            'terabyte' => 10995111627776
+        ],
+        'paths' => [
+            'editor' => ROOT . DS . '..' . DS . 'webroot' . DS . 'public' . DS . 'editor' . DS . 'images' . DS
+        ],
+        'urls' => [
+            'editor' => '/public/editor/images/'
         ]
     ]
 ];
