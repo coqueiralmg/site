ba5ad94323c259493de5e59279ed42f5235cc20b
Fábio Valentim <maitabom@gmail.com>
2017-08-24 16:07:51 -0300

    Adequação do site ao novo padrão de configuração

---
+++ config/pagination.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'Pagination' => [
+        'limit' => 10,
+        'modulus' => 6,
+        'visiblePages' => 7,
+        'short' => [
+            'limit' => 5,
+            'modulus' => 4,
+            'visiblePages' => 5
+        ]
+    ],
+];
--- config/system.php
+++ config/system.php
@@ -1,15 +1,15 @@
 <?php
 
 return[
-    'limitPagination' => 10,
-    'system' => [
+    'System' => [
         'name'=> 'Prefeitura Municipal de Coqueiral',
         'shortName' => 'Prefeitura de Coqueiral',
         'version' => '1.0',
         'fullVersion' => '1.0.0',
+        'yearCreation' => '2017',
         'yearRelease' => '2017'
     ],
-    'author' => [
+    'Author' => [
         'name' => 'Prefeitura Municipal de Coqueiral',
         'company' => 'Prefeitura Municipal de Coqueiral',
         'local' => 'Coqueiral - MG',
@@ -17,11 +17,11 @@ return[
         'email' => 'ti@coqueiral.mg.gov.br',
         'developer' => [
             'name' => 'Fábio Valentim',
-            'site' => 'www.baudovalentim.net',
+            'site' => 'http://www.baudovalentim.net',
             'email' => 'valentim@baudovalentim.net'
         ]
     ],
-    'host' => [
+    'Host' => [
         'developer' => 'localhost:81',
         'release' => 'coqueiral.mg.gov.br'
     ]
