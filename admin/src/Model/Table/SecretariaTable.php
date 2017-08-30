01aab27ef1e16ad74a0a457f1de4ad68f3f7bafe
Fábio Valentim <maitabom@gmail.com>
2017-08-30 14:13:07 -0300

    Criação de entidade e tabela de secretarias

---
+++ admin/src/Model/Table/SecretariaTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model\Table;
+
+
+class SecretariaTable extends BaseTable
+{
+    public function initialize(array $config)
+    {
+        $this->table('secretaria');
+        $this->primaryKey('id');
+    }
+}
