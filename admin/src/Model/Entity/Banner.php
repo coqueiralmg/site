fff59c3911eaabcb7e3b90f1ec97b522235a0bbc
Fábio Valentim <maitabom@gmail.com>
2017-09-05 10:33:49 -0300

    Criação de propriedades virtuais para listagem de banners

--- admin/src/Model/Entity/Banner.php
+++ admin/src/Model/Entity/Banner.php
@@ -7,5 +7,20 @@ use Cake\ORM\Entity;
 
 class Banner extends Entity
 {
+    protected function _getLink()
+    {
+        $destino = $this->_properties['destino'];
+        
+        if($destino == null || $destino == '')
+        {
+            $destino = 'Nenhum';
+        }
+
+        return $destino;
+    }
     
+    protected function _getAtivado()
+    {
+        return $this->_properties['ativo'] ? 'Sim' : 'Não';
+    }
 }
