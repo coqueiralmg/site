de1fa94c1ea0f4054cc87119c07064b07bab0372
Fábio Valentim <maitabom@gmail.com>
2017-09-06 10:23:45 -0300

    Mudança do nome da chave de tipo de arquivo, para implementação de arquivo de banner

--- admin/config/files.php
+++ admin/config/files.php
@@ -24,14 +24,14 @@ return [
             'publicacoes' => ROOT . DS . '..' . DS . 'webroot' . DS . 'public' . DS . 'storage' . DS . 'legislacao-arquivo' . DS,
             'licitacoes' => ROOT . DS . '..' . DS . 'webroot' . DS . 'public' . DS . 'storage' . DS . 'licitacao-edital' . DS,
             'noticias' => ROOT . DS . '..' . DS . 'webroot' . DS . 'public' . DS . 'storage' . DS . 'noticia-foto' . DS . 'large' . DS,
-            'banner_home' => ROOT . DS . '..' . DS . 'webroot' . DS . 'public' . DS . 'slides' . DS . 'home' . DS
+            'bannerHome' => ROOT . DS . '..' . DS . 'webroot' . DS . 'public' . DS . 'slides' . DS . 'home' . DS
         ],
         'urls' => [
             'editor' => '/public/editor/images/',
             'publicacoes' => 'public/storage/legislacao-arquivo/',
             'licitacoes' => 'public/storage/licitacao-edital/',
             'noticias' => 'public/storage/noticia-foto/large/',
-            'banner_home' => 'public/slides/home'
+            'bannerHome' => 'public/slides/home'
         ]
     ]
 ];
