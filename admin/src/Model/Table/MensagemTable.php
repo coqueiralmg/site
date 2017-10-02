e0da334b45d6b443d88b41698e5676680ac482f2
Fábio Valentim <maitabom@gmail.com>
2017-10-02 09:27:38 -0300

    Criação da entidade e tabela de mensagem

---
+++ admin/src/Model/Table/MensagemTable.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Model\Table;
+
+
+class MensagemTable extends BaseTable
+{
+    public function initialize(array $config)
+    {
+        $this->table('mensagem');
+        $this->primaryKey('id');
+        $this->entityClass('Mensagem');
+
+        $this->belongsTo('Usuario', [
+            'className' => 'Usuario',
+            'foreignKey' => 'rementente',
+            'propertyName' => 'rementente',
+            'joinType' => 'LEFT OUTER'
+        ]);
+
+        $this->belongsTo('Usuario', [
+            'className' => 'Usuario',
+            'foreignKey' => 'destinatario',
+            'propertyName' => 'destinatario',
+            'joinType' => 'LEFT OUTER'
+        ]);
+    }
+}
