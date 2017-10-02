024d2eadd04c6fa61aad61a38534665a26170858
Fábio Valentim <maitabom@gmail.com>
2017-10-02 13:06:27 -0300

    Criação da tela de enviados

--- admin/src/Controller/MensagensController.php
+++ admin/src/Controller/MensagensController.php
@@ -44,4 +44,31 @@ class MensagensController extends AppController
         $this->set('qtd_total', $quantidade);
         $this->set('limit_pagination', $limite_paginacao);
     }
+
+    public function enviados()
+    {
+        $t_mensagens = TableRegistry::get('Mensagem');
+        $limite_paginacao = Configure::read('Pagination.limit');
+
+        $conditions = [
+            'rementente' =>  $this->request->session()->read('UsuarioID')
+        ];
+
+        $this->paginate = [
+            'limit' => $limite_paginacao,
+            'conditions' => $conditions,
+            'order' => [
+                'data' => 'DESC'
+            ]
+        ];
+
+        $mensagens = $this->paginate($t_mensagens);
+        $quantidade = $t_mensagens->find('all', ['conditions' => $conditions])->count();
+        
+        $this->set('title', 'Mensagens');
+        $this->set('icon', 'mail_outline');
+        $this->set('mensagens', $mensagens);
+        $this->set('qtd_total', $quantidade);
+        $this->set('limit_pagination', $limite_paginacao);
+    }
 }
