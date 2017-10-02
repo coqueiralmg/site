6d4e6bec60bc3b5dce4e823d8c7ad1cdacd44b8b
Fábio Valentim <maitabom@gmail.com>
2017-10-02 12:50:21 -0300

    Criação do controller para mensagens

---
+++ admin/src/Controller/MensagensController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use Cake\Core\Configure;
+use Cake\Network\Session;
+use Cake\ORM\TableRegistry;
+
+
+class MensagensController extends AppController
+{
+    public function initialize()
+    {
+        parent::initialize();
+
+        $this->validationRole = false;
+        $this->controlAuth();
+        $this->carregarDadosSistema();
+    }
+
+    public function index()
+    {
+        $t_mensagens = TableRegistry::get('Mensagem');
+        $limite_paginacao = Configure::read('Pagination.limit');
+
+        $conditions = [
+            'destinatario' =>  $this->request->session()->read('UsuarioID')
+        ];
+
+        $this->paginate = [
+            'limit' => $limite_paginacao,
+            'conditions' => $conditions,
+            'order' => [
+                'data' => 'DESC'
+            ]
+        ];
+
+        $mensagens = $this->paginate($t_mensagens);
+        $quantidade = $t_mensagens->find('all', ['conditions' => $conditions])->count();
+        
+        $this->set('title', 'Mensagens');
+        $this->set('icon', 'mail_outline');
+        $this->set('mensagens', $mensagens);
+        $this->set('qtd_total', $quantidade);
+        $this->set('limit_pagination', $limite_paginacao);
+    }
+}
