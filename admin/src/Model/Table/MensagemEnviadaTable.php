4c90a646f81e990b0001e239739ecde458c11219
Fábio Valentim <maitabom@gmail.com>
2017-10-03 09:36:46 -0300

    Criação de tabela para mensagens enviadas

---
+++ admin/src/Model/Table/MensagemEnviadaTable.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Model\Table;
+
+
+class MensagemEnviadaTable extends BaseTable
+{
+    public function initialize(array $config)
+    {
+        $this->table('mensagem');
+        $this->primaryKey('id');
+        $this->entityClass('Mensagem');
+
+        
+        $this->belongsTo('Usuario', [
+            'className' => 'Usuario',
+            'foreignKey' => 'destinatario',
+            'propertyName' => 'destinatario',
+            'joinType' => 'LEFT OUTER'
+        ]);
+        
+    }
+}
