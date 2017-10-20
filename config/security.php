5451fbcb5f60ab910cc486452739b76832e2ab9f
Fábio Valentim <maitabom@gmail.com>
2017-10-20 15:39:45 -0200

    Criação de configurações relativas ao Google Captcha

--- config/security.php
+++ config/security.php
@@ -10,6 +10,17 @@ return[
      */
     'Security' => [
         'salt' => env('SECURITY_SALT', 'UHJlZmVpdHVyYSBNdW5pY2lwYWwgZGUgQ29xdWVpcmFsIC0gQW1vciBQb3IgTm9zc2EgR2VudGU='),
+        'reCaptcha' => [
+            'urlVerify' => 'https://www.google.com/recaptcha/api/siteverify',
+            'default' => [
+                'siteKey' => '6LeLIjUUAAAAACJAPVBLYecOJhY1tNA2g1uFQ63m',
+                'secretKey' => '6LeLIjUUAAAAAAR5Q18vvucNk8oM7qCaFhfu57qZ'
+            ],
+            'invisible' => [
+                'siteKey' => '6LfbJjUUAAAAAP7GFrCMqGaz8BaexqXmYha6ozbV',
+                'secretKey' => '6LfbJjUUAAAAAKaq5HkD-9nqZNvlVHJz7or9E3eC'
+            ]
+        ],
         'login' => [
             'attemps' => [
                 'max' => 5,
