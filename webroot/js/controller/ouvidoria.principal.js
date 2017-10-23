a23a8b82499ab577c186bef811b26f1b4804fc82
Fábio Valentim <maitabom@gmail.com>
2017-10-23 10:10:24 -0200

    Realocação da funcão de JavaScript de envio de mensagem de auditoria

--- webroot/js/controller/ouvidoria.principal.js
+++ webroot/js/controller/ouvidoria.principal.js
@@ -1,3 +1,19 @@
+function enviarMensagem() {
+    var response = grecaptcha.getResponse(); 
+
+    if(response == null || response == ""){
+       swal(
+           'Atenção',
+           'Por favor, prove à Prefeitura de Coqueiral de que você não é um robô.',
+           'warning'
+       );	
+
+       return false;
+    }
+
+    return true;
+}
+
 function onSubmit(token)
 {
     document.getElementById("main-contact-form").submit();	
