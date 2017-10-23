4dc106f33e1c6f660de5f381be1bff6045844449
Fábio Valentim <maitabom@gmail.com>
2017-10-23 09:34:00 -0200

    Criação de ClientControllers para o sistema de ouvidoria

---
+++ webroot/js/controller/ouvidoria.acesso.js
@@ -0,0 +1,4 @@
+function onSubmit(token)
+{
+    document.getElementById("frm_logon").submit();	
+}
---
+++ webroot/js/controller/ouvidoria.principal.js
@@ -0,0 +1,4 @@
+function onSubmit(token)
+{
+    document.getElementById("main-contact-form").submit();	
+}
