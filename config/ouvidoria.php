a096acf8a91f3e00bfae5f2cb529f6970836dc3f
Fábio Valentim <maitabom@gmail.com>
2017-10-24 13:36:16 -0200

    Adição de configurações a respeito de definições de ouvidoria

--- config/ouvidoria.php
+++ config/ouvidoria.php
@@ -6,7 +6,16 @@ return[
         'grupoOuvidor'=> 7,
         'status' => [
             'inicial' => 1,
-            'fechado' => 6
+            'fechado' => 6,
+            'definicoes' => [
+                'novo' => 1,
+                'aceito' => 2,
+                'atendido' => 3,
+                'emAtividade' => 4,
+                'concluido' => 5,
+                'fechado' => 6,
+                'recusado' => 7
+            ]
         ],
         'prioridadeInicial' => 1,
         'prazo' => 10,
