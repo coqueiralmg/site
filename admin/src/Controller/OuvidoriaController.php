0cf21c23481feddadb7d1be3ec0857d1cf3b8748
Fábio Valentim <maitabom@gmail.com>
2017-10-25 10:53:18 -0200

    Criação do controller de Ouvidoria

---
+++ admin/src/Controller/OuvidoriaController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Cake\Core\Configure;
+use Cake\ORM\TableRegistry;
+
+
+class OuvidoriaController extends AppController
+{
+    public function initialize()
+    {
+        parent::initialize();
+    }
+
+    public function index()
+    {
+    }
+
+    public function imprimir()
+    {
+        
+    }
+}
