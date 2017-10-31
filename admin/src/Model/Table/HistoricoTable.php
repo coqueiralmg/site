c07ac6cf4a456aae3721342b6c0f5091ff148c31
Fábio Valentim <maitabom@gmail.com>
2017-10-31 08:52:07 -0200

    Criação da associação entre histórico, status e prioridade

--- admin/src/Model/Table/HistoricoTable.php
+++ admin/src/Model/Table/HistoricoTable.php
@@ -10,5 +10,19 @@ class HistoricoTable extends BaseTable
         $this->table('historico');
         $this->primaryKey('id');
         $this->entityClass('Historico');
+
+        $this->belongsTo('Prioridade', [
+            'className' => 'Prioridade',
+            'foreignKey' => 'prioridade',
+            'propertyName' => 'prioridade',
+            'joinType' => 'INNER'
+        ]);
+
+        $this->belongsTo('Status', [
+            'className' => 'Status',
+            'foreignKey' => 'status',
+            'propertyName' => 'status',
+            'joinType' => 'INNER'
+        ]);
     }
 }
