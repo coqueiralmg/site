669c67aae22e585d1d07e7b0cb8b4d71fdf91e70
Fábio Valentim <maitabom@gmail.com>
2018-01-08 10:41:23 -0200

    Criação das funcionalidades de bloqueio e desbloqueio de manifestante, no controller de ouvidoria

--- admin/src/Controller/ManifestanteController.php
+++ admin/src/Controller/ManifestanteController.php
@@ -22,4 +22,46 @@ class ManifestanteController extends AppController
             '_serialize' => ['manifestante']
         ]);
     }
+
+    public function ban()
+    {
+        if ($this->request->is('post'))
+        {
+            $id = $this->request->getData('id');
+            
+            $t_manifestante = TableRegistry::get('Manifestante');
+            $manifestante = $t_manifestante->get($id);
+
+            $manifestante->bloqueado = true;
+
+            $t_manifestante->save($manifestante);
+
+            $this->set([
+                'sucesso' => true,
+                'manifestante' => $manifestante,
+                '_serialize' => ['sucesso', 'manifestante']
+            ]);
+        }
+    }
+
+    public function release()
+    {
+        if ($this->request->is('post'))
+        {
+            $id = $this->request->getData('id');
+            
+            $t_manifestante = TableRegistry::get('Manifestante');
+            $manifestante = $t_manifestante->get($id);
+
+            $manifestante->bloqueado = false;
+
+            $t_manifestante->save($manifestante);
+
+            $this->set([
+                'sucesso' => true,
+                'manifestante' => $manifestante,
+                '_serialize' => ['sucesso', 'manifestante']
+            ]);
+        }
+    }
 }
