b7f70f236bc036bb64886d1f35500096aa2e4619
Fábio Valentim <maitabom@gmail.com>
2018-01-09 08:38:51 -0200

    Ajustes na tabela de mensagem, prevendo o OUTER JOIN na busca de mensagens com usuários

--- admin/src/Model/Table/MensagemTable.php
+++ admin/src/Model/Table/MensagemTable.php
@@ -11,8 +11,8 @@ class MensagemTable extends BaseTable
         $this->primaryKey('id');
         $this->entityClass('Mensagem');
 
-        $this->belongsTo('Usuario', [
-            'className' => 'Usuario',
+        $this->belongsTo('Rementente', [
+            'className' => 'Rementente',
             'foreignKey' => 'rementente',
             'propertyName' => 'rementente',
             'joinType' => 'LEFT OUTER'
