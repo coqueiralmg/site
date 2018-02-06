0118bed834ea575854d8c744f9dad0171a517786
Fábio Valentim <maitabom@gmail.com>
2018-02-06 14:57:00 -0200

    Descomentação da biblioteca de demonstração de template padrão

--- admin/webroot/js/demo.js
+++ admin/webroot/js/demo.js
@@ -32,116 +32,116 @@ demo = {
         });
     },
 
-    // initDocumentationCharts: function () {
-    //     /* ----------==========     Daily Sales Chart initialization For Documentation    ==========---------- */
+    initDocumentationCharts: function () {
+        /* ----------==========     Daily Sales Chart initialization For Documentation    ==========---------- */
 
-    //     dataDailySalesChart = {
-    //         labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-    //         series: [
-    //             [12, 17, 7, 17, 23, 18, 38]
-    //         ]
-    //     };
+        dataDailySalesChart = {
+            labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+            series: [
+                [12, 17, 7, 17, 23, 18, 38]
+            ]
+        };
 
-    //     optionsDailySalesChart = {
-    //         lineSmooth: Chartist.Interpolation.cardinal({
-    //             tension: 0
-    //         }),
-    //         low: 0,
-    //         high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-    //         chartPadding: { top: 0, right: 0, bottom: 0, left: 0 },
-    //     }
+        optionsDailySalesChart = {
+            lineSmooth: Chartist.Interpolation.cardinal({
+                tension: 0
+            }),
+            low: 0,
+            high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+            chartPadding: { top: 0, right: 0, bottom: 0, left: 0 },
+        }
 
-    //     var dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
+        var dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
 
-    //     md.startAnimationForLineChart(dailySalesChart);
-    // },
+        md.startAnimationForLineChart(dailySalesChart);
+    },
 
-    // initDashboardPageCharts: function(){
-
-
-    //     /* ----------==========     Daily Sales Chart initialization    ==========---------- */
-
-    //     dataDailySalesChart = {
-    //         labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-    //         series: [
-    //             [12, 17, 7, 17, 23, 18, 38]
-    //         ]
-    //     };
+    initDashboardPageCharts: function(){
 
 
-    //     optionsDailySalesChart = {
-    //         lineSmooth: Chartist.Interpolation.cardinal({
-    //             tension: 0
-    //         }),
-    //         low: 0,
-    //         high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-    //         chartPadding: { top: 0, right: 0, bottom: 0, left: 0},
-    //     }
+        /* ----------==========     Daily Sales Chart initialization    ==========---------- */
 
-    //     var dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
-
-    //     md.startAnimationForLineChart(dailySalesChart);
+        dataDailySalesChart = {
+            labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+            series: [
+                [12, 17, 7, 17, 23, 18, 38]
+            ]
+        };
 
 
+        optionsDailySalesChart = {
+            lineSmooth: Chartist.Interpolation.cardinal({
+                tension: 0
+            }),
+            low: 0,
+            high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+            chartPadding: { top: 0, right: 0, bottom: 0, left: 0},
+        }
 
-    //     /* ----------==========     Completed Tasks Chart initialization    ==========---------- */
+        var dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
 
-    //     dataCompletedTasksChart = {
-    //         labels: ['12am', '3pm', '6pm', '9pm', '12pm', '3am', '6am', '9am'],
-    //         series: [
-    //             [230, 750, 450, 300, 280, 240, 200, 190]
-    //         ]
-    //     };
-
-    //     optionsCompletedTasksChart = {
-    //         lineSmooth: Chartist.Interpolation.cardinal({
-    //             tension: 0
-    //         }),
-    //         low: 0,
-    //         high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-    //         chartPadding: { top: 0, right: 0, bottom: 0, left: 0}
-    //     }
-
-    //     var completedTasksChart = new Chartist.Line('#completedTasksChart', dataCompletedTasksChart, optionsCompletedTasksChart);
-
-    //     // start animation for the Completed Tasks Chart - Line Chart
-    //     md.startAnimationForLineChart(completedTasksChart);
+        md.startAnimationForLineChart(dailySalesChart);
 
 
 
-    //     /* ----------==========     Emails Subscription Chart initialization    ==========---------- */
+        /* ----------==========     Completed Tasks Chart initialization    ==========---------- */
 
-    //     var dataEmailsSubscriptionChart = {
-    //       labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-    //       series: [
-    //         [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]
+        dataCompletedTasksChart = {
+            labels: ['12am', '3pm', '6pm', '9pm', '12pm', '3am', '6am', '9am'],
+            series: [
+                [230, 750, 450, 300, 280, 240, 200, 190]
+            ]
+        };
 
-    //       ]
-    //     };
-    //     var optionsEmailsSubscriptionChart = {
-    //         axisX: {
-    //             showGrid: false
-    //         },
-    //         low: 0,
-    //         high: 1000,
-    //         chartPadding: { top: 0, right: 5, bottom: 0, left: 0}
-    //     };
-    //     var responsiveOptions = [
-    //       ['screen and (max-width: 640px)', {
-    //         seriesBarDistance: 5,
-    //         axisX: {
-    //           labelInterpolationFnc: function (value) {
-    //             return value[0];
-    //           }
-    //         }
-    //       }]
-    //     ];
-    //     var emailsSubscriptionChart = Chartist.Bar('#emailsSubscriptionChart', dataEmailsSubscriptionChart, optionsEmailsSubscriptionChart, responsiveOptions);
+        optionsCompletedTasksChart = {
+            lineSmooth: Chartist.Interpolation.cardinal({
+                tension: 0
+            }),
+            low: 0,
+            high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+            chartPadding: { top: 0, right: 0, bottom: 0, left: 0}
+        }
 
-    //     //start animation for the Emails Subscription Chart
-    //     md.startAnimationForBarChart(emailsSubscriptionChart);
+        var completedTasksChart = new Chartist.Line('#completedTasksChart', dataCompletedTasksChart, optionsCompletedTasksChart);
 
-    // },
+        // start animation for the Completed Tasks Chart - Line Chart
+        md.startAnimationForLineChart(completedTasksChart);
+
+
+
+        /* ----------==========     Emails Subscription Chart initialization    ==========---------- */
+
+        var dataEmailsSubscriptionChart = {
+          labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+          series: [
+            [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]
+
+          ]
+        };
+        var optionsEmailsSubscriptionChart = {
+            axisX: {
+                showGrid: false
+            },
+            low: 0,
+            high: 1000,
+            chartPadding: { top: 0, right: 5, bottom: 0, left: 0}
+        };
+        var responsiveOptions = [
+          ['screen and (max-width: 640px)', {
+            seriesBarDistance: 5,
+            axisX: {
+              labelInterpolationFnc: function (value) {
+                return value[0];
+              }
+            }
+          }]
+        ];
+        var emailsSubscriptionChart = Chartist.Bar('#emailsSubscriptionChart', dataEmailsSubscriptionChart, optionsEmailsSubscriptionChart, responsiveOptions);
+
+        //start animation for the Emails Subscription Chart
+        md.startAnimationForBarChart(emailsSubscriptionChart);
+
+    },
 
     initGoogleMaps: function () {
         var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
