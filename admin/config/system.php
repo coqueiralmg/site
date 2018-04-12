641d6cd298dc48d0898754abd5f5ae30edef1633
Fábio Valentim <maitabom@gmail.com>
2018-04-12 14:26:46 -0300

    Definição da nova versão do sistema e do site

--- admin/config/system.php
+++ admin/config/system.php
@@ -5,7 +5,7 @@ return[
         'name'=> 'Painel de Controle | Prefeitura Municipal de Coqueiral',
         'shortName' => 'Prefeitura de Coqueiral',
         'version' => '1.1',
-        'fullVersion' => '1.1.0',
+        'fullVersion' => '1.1.1',
         'yearCreation' => '2017',
         'yearRelease' => '2018'
     ],
