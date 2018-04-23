8b0795563f2a899610435e6d672c812342b9a7ce
Fábio Valentim <maitabom@gmail.com>
2018-04-23 11:03:07 -0300

    Criação de controller, tabela e entidade relacionado a diárias

---
+++ admin/src/Controller/DiariasController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Cake\Core\Configure;
+use Cake\Network\Session;
+use Cake\ORM\TableRegistry;
+
+
+class DiariasController extends AppController
+{
+    public function initialize()
+    {
+        parent::initialize();
+    }
+
+    public function index()
+    {
+
+    }
+}
