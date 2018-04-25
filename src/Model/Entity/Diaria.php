3fb715119cf30725b7c0a7c5581bf6b1ff0eff4f
Fábio Valentim <maitabom@gmail.com>
2018-04-25 13:31:32 -0300

    Adição de propriedade que mostra texto resumido de objetivo de viagem de diária

--- src/Model/Entity/Diaria.php
+++ src/Model/Entity/Diaria.php
@@ -10,4 +10,22 @@ class Diaria extends Entity
     {
         return $this->_properties['ativo'] ? 'Sim' : 'Não';
     }
+
+    protected function _getResumo()
+    {
+        $limiteCaracteres = 250;
+        $texto = $this->_properties['objetivo'];
+        $texto = strip_tags($texto);
+        $reticences = "";
+
+        if (strlen($texto) > $limiteCaracteres){
+            $reticences = "...";
+            $limite = substr($texto, 0, $limiteCaracteres);
+            $posicaoString = strrpos($limite, " ");
+            $cortaTexto = ($posicaoString > 0) ? $posicaoString : strlen($limite);
+            $texto = substr($limite, 0, $cortaTexto);
+        }
+
+        return $texto . $reticences;
+    }
 }
