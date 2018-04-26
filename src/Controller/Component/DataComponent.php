2540553693c5b4454b10899dd6732c48d56bf0e9
Fábio Valentim <maitabom@gmail.com>
2018-04-26 14:38:47 -0300

    Criação do componente de criptografia de dados e documentação do sistema

--- src/Controller/Component/DataComponent.php
+++ src/Controller/Component/DataComponent.php
@@ -4,8 +4,16 @@ namespace App\Controller\Component;
 
 use Cake\Controller\Component;
 
+/**
+ * Componente de manipulação de dados, metadados e informações de sistema
+ */
 class DataComponent extends Component
 {
+    /**
+     * Codifica um dado ou uma coleção de dados
+     * @param string $data String chave com as informações criptografadas de dados.
+     * @return Informação decriptografada de banco de dados.
+     */
     public function decrypt(string $data)
     {
         return json_decode(base64_decode($data));
