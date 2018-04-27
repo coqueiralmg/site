92a4454781cc6266f6951e0451006b4855429562
Fábio Valentim <maitabom@gmail.com>
2018-04-27 13:39:46 -0300

    Criação da função que busca Cookie do sistema

--- admin/webroot/js/default.js
+++ admin/webroot/js/default.js
@@ -23,3 +23,19 @@ function imprimeRelogio() {
 
     setTimeout("imprimeRelogio()", 1000);
 }
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
