f4e3d74298f61db5ecb5be5cffab7dd3d6d370ba
Fábio Valentim <maitabom@gmail.com>
2018-05-02 14:03:03 -0300

    Adaptação da tela de cadatro, para a funcionalidade do autosave

--- admin/webroot/js/controller/firewall.cadastro.js
+++ admin/webroot/js/controller/firewall.cadastro.js
@@ -1,6 +1,53 @@
 $(function () {
+    $('#ip').mask('099.099.099.099');
 
+    $("input, textarea").change(function(){
+        autosave();
+    });
+
+    if(hasCache('firewall', idRegistro)) {
+        $("#cadastro_info").show('fade');
+    }
 });
+
+function restaurar() {
+    var data = getDataCache('firewall', idRegistro);
+
+    if (data != null) {
+        $("#ip").val(data.object.ip);
+        $("#motivo").val(data.object.motivo);
+        $("#lista_branca").prop("checked", data.object.listaBranca);
+        $("#ativo").prop("checked", data.object.ativo);
+        $("#site").prop("checked", data.object.site);
+    }
+
+    notificarUsuario("Os dados em cache foram restaurados com sucesso!", "success")
+}
+
+function cancelarRestauracao() {
+    removeCache();
+    notificarUsuario("Você acabou de descartar dados que estão em cache.", "warning")
+}
+
+function autosave() {
+    var data = {
+        id: idRegistro,
+        object: {
+            id: idRegistro,
+            ip: $("#ip").val(),
+            motivo: $("#motivo").val(),
+            listaBranca: $("#lista_branca").is(':checked'),
+            ativo: $("#ativo").is(':checked'),
+            site: $("#site").is(':checked')
+        }
+    };
+
+    cacheSave('firewall', data);
+}
+
+function removeCache() {
+    removeData('firewall', idRegistro);
+}
 
 function validar() {
     var mensagem = "";
