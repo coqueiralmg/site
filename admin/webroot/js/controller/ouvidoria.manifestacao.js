804cbab617b940ac6c52422833c778780ed81998
Fábio Valentim <maitabom@gmail.com>
2018-05-03 10:06:28 -0300

    Adaptação da tela de ouvidoria para a funcionalidade de autosave

--- admin/webroot/js/controller/ouvidoria.manifestacao.js
+++ admin/webroot/js/controller/ouvidoria.manifestacao.js
@@ -1,3 +1,52 @@
+$(function () {
+    $("input, select, textarea").change(function(){
+        autosave();
+    });
+
+    if(hasCache('manifestacaoOuvidoria', idManifestacao)) {
+        $("#cadastro_info").show('fade');
+    }
+});
+
+function restaurar() {
+    var data = getDataCache('manifestacaoOuvidoria', idManifestacao);
+
+    if (data != null) {
+        $("#resposta").val(data.object.resposta);
+        $("#acao").val(data.object.acao);
+        $("#prioridade").val(data.object.prioridade);
+        $("#notificar").prop("checked", data.object.notificar);
+
+        CKEDITOR.instances.descricao.setData(data.object.descricao);
+    }
+
+    notificarUsuario("Os dados em cache foram restaurados com sucesso!", "success")
+}
+
+function cancelarRestauracao() {
+    removeCache();
+    notificarUsuario("Você acabou de descartar dados que estão em cache.", "warning")
+}
+
+function autosave() {
+    var data = {
+        id: idManifestacao,
+        object: {
+            id: idManifestacao,
+            resposta: $("#resposta").val(),
+            acao: $("#acao").val(),
+            prioridade: $("#prioridade").val(),
+            notificar: $("#notificar").is(':checked'),
+        }
+    };
+
+    cacheSave('manifestacaoOuvidoria', data);
+}
+
+function removeCache() {
+    removeData('manifestacaoOuvidoria', idManifestacao);
+}
+
 function validar() {
     var mensagem = "";
 
