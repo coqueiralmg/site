635befe219092de3f8aeb1bdb89c3a6dcd125520
Fábio Valentim <maitabom@gmail.com>
2018-05-21 10:51:32 -0300

    Implemntação da funcionalidade de validação de menu ativo na tela de concursos

--- admin/src/View/Helper/MenuHelper.php
+++ admin/src/View/Helper/MenuHelper.php
@@ -31,4 +31,29 @@ class MenuHelper extends Helper
 
         return $style;
     }
+
+     /**
+     * Valida o menu ativo do sistema
+     * @param array $location Localização pivot do sistema
+     * @return bool Se vai retornar o menu ativo.
+     */
+    public function activeMenuItem(array $location)
+    {
+        $active = false;
+        $controller = strtolower($this->request->getParam('controller'));
+        $action = strtolower($this->request->getParam('action'));
+
+        if(isset($location['action']))
+        {
+            if($controller == $location['controller'] && $action == $location['action'])
+                $active = true;
+        }
+        else
+        {
+            if($controller == $location['controller'])
+                $active = true;
+        }
+
+        return $active;
+    }
 }
