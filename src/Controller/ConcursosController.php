3f22a370b7684421b96da6b2527d11bb85030cb5
Fábio Valentim <maitabom@gmail.com>
2018-06-18 15:44:49 -0300

    Desenvolvimento da Action para listagem de concurso público

--- src/Controller/ConcursosController.php
+++ src/Controller/ConcursosController.php
@@ -9,6 +9,33 @@ class ConcursosController extends AppController
 {
     public function index()
     {
+        $t_concursos = TableRegistry::get('Concurso');
+        $t_informativo = TableRegistry::get('Informativo');
+        $limite_paginacao = Configure::read('Pagination.limit');
 
+        $conditions = array();
+
+        if($this->request->is('get') && count($this->request->query) > 0)
+        {
+            $chave = $this->request->query('chave');
+
+            $conditions['titulo LIKE'] = '%' . $chave . '%';
+
+            $data = array();
+
+            $data['chave'] = $chave;
+
+            $this->request->data = $data;
+        }
+
+        $conditions['ativo'] = true;
+
+        $this->paginate = [
+            'limit' => $limite_paginacao,
+            'conditions' => $conditions,
+            'order' => [
+                'dataProva' => 'DESC'
+            ]
+        ];
     }
 }
