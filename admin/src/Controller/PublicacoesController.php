f20121622a57ebb03aaa0963628bb210de97626b
Fábio Valentim <maitabom@gmail.com>
2018-06-26 09:08:16 -0300

    Criação de controller para publicações

---
+++ admin/src/Controller/PublicacoesController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use Cake\Core\Configure;
+use Cake\Network\Session;
+use Cake\ORM\TableRegistry;
+use \Exception;
+
+class PublicacoesController extends AppController
+{
+
+}
