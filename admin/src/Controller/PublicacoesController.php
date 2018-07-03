60b2fab33fef2b68b68fe8abc3ad782dfa052cf0
Fábio Valentim <maitabom@gmail.com>
2018-07-03 10:14:46 -0300

    Implementação da busca de publicações

--- admin/src/Controller/PublicacoesController.php
+++ admin/src/Controller/PublicacoesController.php
@@ -16,6 +16,80 @@ class PublicacoesController extends AppController
 
     public function index()
     {
+        $t_publicacoes = TableRegistry::get('Publicacao');
+        $limite_paginacao = Configure::read('Pagination.limit');
 
+        $condicoes = array();
+        $data = array();
+
+        if (count($this->request->getQueryParams()) > 3)
+        {
+            $numero = $this->request->query('numero');
+            $titulo = $this->request->query('titulo');
+            $data_inicial = $this->request->query('data_inicial');
+            $data_final = $this->request->query('data_final');
+            $mostrar = $this->request->query('mostrar');
+
+            if($numero != "")
+            {
+                $condicoes['numero LIKE'] = '%' . $numero . '%';
+            }
+
+            if($titulo != "")
+            {
+                $condicoes['titulo LIKE'] = '%' . $titulo . '%';
+            }
+
+            if ($data_inicial != "" && $data_final != "")
+            {
+                $condicoes["data >="] = $this->Format->formatDateDB($data_inicial);
+                $condicoes["data <="] = $this->Format->formatDateDB($data_final);
+            }
+
+            if ($mostrar != 'T')
+            {
+                $condicoes["ativo"] = ($mostrar == "A") ? "1" : "0";
+            }
+
+            $data['numero'] = $numero;
+            $data['titulo'] = $titulo;
+            $data['data_inicial'] = $data_inicial;
+            $data['data_final'] = $data_final;
+            $data['mostrar'] = $mostrar;
+
+            $this->request->data = $data;
+        }
+
+        $this->paginate = [
+            'limit' => $limite_paginacao,
+            'conditions' => $condicoes,
+            'order' => [
+                'data' => 'DESC'
+            ]
+        ];
+
+        $opcao_paginacao = [
+            'name' => 'publicações',
+            'name_singular' => 'publicação',
+            'predicate' => 'encontradas',
+            'singular' => 'encontrada'
+        ];
+
+        $combo_mostra = ["T" => "Todos", "A" => "Somente ativos", "I" => "Somente inativos"];
+
+        $publicacoes = $this->paginate($t_publicacoes);
+
+        $qtd_total = $t_publicacoes->find('all', [
+            'conditions' => $condicoes]
+        )->count();
+
+        $this->set('title', 'Publicações');
+        $this->set('icon', 'insert_drive_file');
+        $this->set('combo_mostra', $combo_mostra);
+        $this->set('publicacoes', $publicacoes);
+        $this->set('qtd_total', $qtd_total);
+        $this->set('limit_pagination', $limite_paginacao);
+        $this->set('opcao_paginacao', $opcao_paginacao);
+        $this->set('data', $data);
     }
 }
