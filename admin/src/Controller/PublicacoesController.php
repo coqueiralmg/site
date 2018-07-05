80c944ae1e55abd2376082e59bf6c771018bd50f
Fábio Valentim <maitabom@gmail.com>
2018-07-05 08:37:41 -0300

    Criação de métodos para inclusão e alterações de publicações

--- admin/src/Controller/PublicacoesController.php
+++ admin/src/Controller/PublicacoesController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use Cake\Core\Configure;
+use Cake\Filesystem\Folder;
+use Cake\Filesystem\File;
 use Cake\Network\Session;
 use Cake\ORM\TableRegistry;
 use \Exception;
+use \DateTime;
 
 class PublicacoesController extends AppController
 {
@@ -123,5 +126,178 @@ class PublicacoesController extends AppController
         $this->set('title', $title);
         $this->set('icon', 'insert_drive_file');
         $this->set('id', $id);
+    }
+
+    public function save(int $id)
+    {
+        if ($this->request->is('post'))
+        {
+            $this->insert();
+        }
+        elseif ($this->request->is('put'))
+        {
+            $this->update($id);
+        }
+    }
+
+    protected function insert()
+    {
+        try
+        {
+            $t_publicacoes = TableRegistry::get('Publicacao');
+            $entity = $t_publicacoes->newEntity($this->request->data());
+
+            if($entity->hora == '')
+            {
+                $pivot = new DateTime();
+                $hora = $pivot->format('H:i:s');
+            }
+            else
+            {
+                $hora = $entity->hora;
+            }
+
+            $entity->data = $this->Format->mergeDateDB($entity->data, $hora);
+
+            $arquivo = $this->request->getData('arquivo');
+            $entity->arquivo = $this->salvarArquivo($arquivo);
+
+            $t_publicacoes->save($entity);
+            $this->Flash->greatSuccess('A publicação foi salva com sucesso.');
+
+            $propriedades = $entity->getOriginalValues();
+
+            $auditoria = [
+                'ocorrencia' => 21,
+                'descricao' => 'O usuário criou uma nova publicação.',
+                'dado_adicional' => json_encode(['id_nova_publicacao' => $entity->id, 'dados_publicacao' => $propriedades]),
+                'usuario' => $this->request->session()->read('UsuarioID')
+            ];
+
+            $this->Auditoria->registrar($auditoria);
+
+            if($this->request->session()->read('UsuarioSuspeito'))
+            {
+                $this->Monitoria->monitorar($auditoria);
+            }
+
+            $this->redirect(['action' => 'cadastro', $entity->id]);
+        }
+        catch(Exception $ex)
+        {
+            $this->Flash->exception('Ocorreu um erro no sistema ao salvar a publicação', [
+                'params' => [
+                    'details' => $ex->getMessage()
+                ]
+            ]);
+
+            $this->redirect(['action' => 'cadastro', 0]);
+        }
+    }
+
+    protected function update(int $id)
+    {
+        try
+        {
+            $t_publicacoes = TableRegistry::get('Publicacao');
+            $entity = $t_publicacoes->get($id);
+
+            $antigo_arquivo = $entity->arquivo;
+
+            $t_publicacoes->patchEntity($entity, $this->request->data());
+
+            $entity->data = $this->Format->mergeDateDB($entity->data, $entity->hora);
+            $enviaArquivo = ($this->request->getData('enviaArquivo') == 'true');
+
+            if($enviaArquivo)
+            {
+                $this->removerArquivo($antigo_arquivo);
+                $arquivo = $this->request->getData('arquivo');
+                $entity->arquivo = $this->salvarArquivo($arquivo);
+            }
+            else
+            {
+                $entity->arquivo = $antigo_arquivo;
+            }
+
+            $propriedades = $this->Auditoria->changedOriginalFields($entity);
+            $modificadas = $this->Auditoria->changedFields($entity, $propriedades);
+
+            $t_publicacoes->save($entity);
+            $this->Flash->greatSuccess('A publicação foi salva com sucesso.');
+
+            $auditoria = [
+                'ocorrencia' => 22,
+                'descricao' => 'O usuário editou uma publicação.',
+                'dado_adicional' => json_encode(['publicacao_modificada' => $id, 'valores_originais' => $propriedades, 'valores_modificados' => $modificadas]),
+                'usuario' => $this->request->session()->read('UsuarioID')
+            ];
+
+            $this->Auditoria->registrar($auditoria);
+
+            if($this->request->session()->read('UsuarioSuspeito'))
+            {
+                $this->Monitoria->monitorar($auditoria);
+            }
+
+            $this->redirect(['action' => 'cadastro', $id]);
+        }
+        catch(Exception $ex)
+        {
+            $this->Flash->exception('Ocorreu um erro no sistema ao salvar a publicação', [
+                'params' => [
+                    'details' => $ex->getMessage()
+                ]
+            ]);
+
+            $this->redirect(['action' => 'cadastro', $id]);
+        }
+    }
+
+    private function removerArquivo($arquivo)
+    {
+        $diretorio = Configure::read('Files.paths.public');
+        $arquivo = $diretorio . $arquivo;
+
+        $file = new File($arquivo);
+
+        if($file->exists())
+        {
+            $file->delete();
+        }
+    }
+
+    private function salvarArquivo($arquivo)
+    {
+        $diretorio = Configure::read('Files.paths.publicacao');
+        $url_relativa = Configure::read('Files.urls.publicacao');
+
+        $file_temp = $arquivo['tmp_name'];
+        $nome_arquivo = $arquivo['name'];
+
+        $file = new File($file_temp);
+        $pivot = new File($nome_arquivo);
+
+        $novo_nome = uniqid() . '.' . $pivot->ext();
+
+        if(!$this->File->validationExtension($pivot, $this->File::TYPE_FILE_DOCUMENT))
+        {
+            throw new Exception("A extensão do arquivo é inválida.");
+        }
+        elseif(!$this->File->validationSize($file))
+        {
+            $maximo = $this->File->getMaxLengh($this->File::TYPE_FILE_DOCUMENT);
+            $divisor = Configure::read('Files.misc.megabyte');
+
+            $maximo = round($maximo / $divisor, 0);
+
+            $mensagem = "O tamaho do arquivo enviado é muito grande. O tamanho máximo do arquivo é de $maximo MB.";
+
+            throw new Exception($mensagem);
+        }
+
+        $file->copy($diretorio . $novo_nome, true);
+
+        return $url_relativa . $novo_nome;
     }
 }
