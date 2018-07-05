f485fd44899cfc22df4e22b99da43db3b3742c89
Fábio Valentim <maitabom@gmail.com>
2018-07-05 08:38:52 -0300

    Definição de localização de publicações

--- admin/config/files.php
+++ admin/config/files.php
@@ -22,6 +22,7 @@ return [
             'editor' => ROOT . DS . '..' . DS . 'webroot' . DS . 'public' . DS . 'editor' . DS . 'images' . DS,
             'public' => ROOT . DS . '..' . DS . 'webroot' . DS,
             'legislacao' => ROOT . DS . '..' . DS . 'webroot' . DS . 'public' . DS . 'storage' . DS . 'legislacao-arquivo' . DS,
+            'publicacao' => ROOT . DS . '..' . DS . 'webroot' . DS . 'public' . DS . 'storage' . DS . 'publicacao-arquivo' . DS,
             'licitacoes' => ROOT . DS . '..' . DS . 'webroot' . DS . 'public' . DS . 'storage' . DS . 'licitacao-edital' . DS,
             'diarias' => ROOT . DS . '..' . DS . 'webroot' . DS . 'public' . DS . 'storage' . DS . 'relatorios-diaria' . DS,
             'noticias' => ROOT . DS . '..' . DS . 'webroot' . DS . 'public' . DS . 'storage' . DS . 'noticia-foto' . DS . 'large' . DS,
@@ -31,6 +32,7 @@ return [
         'urls' => [
             'editor' => '/public/editor/images/',
             'legislacao' => 'public/storage/legislacao-arquivo/',
+            'publicacao' => 'public/storage/publicacao-arquivo/',
             'licitacoes' => 'public/storage/licitacao-edital/',
             'diarias' => 'public/storage/relatorios-diaria/',
             'noticias' => 'public/storage/noticia-foto/large/',
