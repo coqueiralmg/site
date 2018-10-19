4a95f820df69fe4b310509a929fb86802b80b5ec
Fábio Valentim <maitabom@gmail.com>
2018-10-19 15:36:40 -0300

    Implementação de busca de legislação por tipo de lei e criação de actions para filtros por assunto ou ano.

--- src/Controller/LegislacaoController.php
+++ src/Controller/LegislacaoController.php
@@ -89,6 +89,101 @@ class LegislacaoController extends AppController
         $this->set('limit_pagination', $limite_paginacao);
     }
 
+    public function tipo(int $id)
+    {
+        $conditions = array();
+        $limite_paginacao = Configure::read('Pagination.limit');
+
+        if($this->request->is('get') && count($this->request->query) > 0)
+        {
+            $chave = $this->request->query('chave');
+
+            $conditions['titulo LIKE'] = '%' . $chave . '%';
+
+            $data = array();
+
+            $data['chave'] = $chave;
+
+            $this->request->data = $data;
+        }
+
+        $conditions['ativo'] = true;
+        $conditions['tipo'] = $id;
+
+        $this->paginate = [
+            'limit' => $limite_paginacao,
+            'conditions' => $conditions,
+            'order' => [
+                'data' => 'DESC'
+            ]
+        ];
+
+        $t_legislacao = TableRegistry::get('Legislacao');
+        $t_tipo_legislacao = TableRegistry::get('TipoLegislacao');
+        $t_assuntos = TableRegistry::get('Assunto');
+
+        $legislacao = $this->paginate($t_legislacao);
+        $inicial = count($this->request->query) == 0;
+        $qtd_total = $t_legislacao->find('all', ['conditions' => $conditions])->count();
+        $tipo_legislacao = $t_tipo_legislacao->get($id);
+
+        $destaques = null;
+        $anos = null;
+        $assuntos = null;
+
+        if($inicial)
+        {
+            $destaques = $t_legislacao->find('destaque', [
+                'conditions' => [
+                    'tipo' => $id
+                ],
+                'order' => [
+                    'data' => 'DESC'
+                ]
+            ]);
+
+            $anos = $t_legislacao->find('ativo', [
+                'conditions' => [
+                    'tipo' => $id
+                ]
+            ]);
+            $anos->select([
+                'ano' => $anos->func()->year(['data' => 'identifier'])
+            ])->group('ano')->order([
+                'ano' => 'DESC'
+            ]);
+
+            $assuntos = $t_assuntos->find('all', [
+                'conditions' => [
+                    'tipo' => 'LG'
+                ],
+                'order' => [
+                    'descricao' => 'ASC'
+                ]
+            ]);
+        }
+
+        $this->set('title', "Legislação");
+        $this->set('legislacao', $legislacao->toArray());
+        $this->set('destaques', $destaques == null ? [] : $destaques->toArray());
+        $this->set('tipo_legislacao', $tipo_legislacao);
+        $this->set('assuntos', $assuntos == null ? [] : $assuntos->toArray());
+        $this->set('anos', $anos == null ? [] : $anos->toArray());
+        $this->set('inicial', $inicial);
+        $this->set('qtd_total', $qtd_total);
+        $this->set('limit_pagination', $limite_paginacao);
+    }
+
+    public function assunto(int $id)
+    {
+
+    }
+
+    public function ano(int $ano)
+    {
+
+    }
+
     public function documento(int $id)
     {
         $t_legislacao = TableRegistry::get('Legislacao');
