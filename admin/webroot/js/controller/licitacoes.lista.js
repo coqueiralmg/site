9ce4d1f24a5ab5a21e9cb0a1280e40f51f290e20
Fábio Valentim <maitabom@gmail.com>
2018-12-04 15:38:33 -0200

    Refatoração do ClientController para atender o novo formato de busca de licitações

--- admin/webroot/js/controller/licitacoes.lista.js
+++ admin/webroot/js/controller/licitacoes.lista.js
@@ -1,42 +1,85 @@
 $(function () {
-    $('#data_inicial').datepicker({
+    $('#data_publicacao_inicial').datepicker({
         language: 'pt-BR'
     });
 
-     $('#data_final').datepicker({
+    $('#data_publicacao_final').datepicker({
         language: 'pt-BR'
     });
 
-    $('#data_inicial').mask('00/00/0000');
-    $('#data_final').mask('00/00/0000');
+    $('#data_sessao_inicial').datepicker({
+        language: 'pt-BR'
+    });
+
+    $('#data_sessao_final').datepicker({
+        language: 'pt-BR'
+    });
+
+    $('#numprocesso').mask('000/0000');
+    $('#data_publicacao_inicial').mask('00/00/0000');
+    $('#data_publicacao_final').mask('00/00/0000');
+    $('#data_sessao_inicial').mask('00/00/0000');
+    $('#data_sessao_final').mask('00/00/0000');
 });
 
 function validar() {
     var mensagem = "";
-    var dataInicial = $('#data_inicial').val();
-    var dataFinal = $('#data_final').val();
+    var dataPublicacaoInicial = $('#data_publicacao_inicial').val();
+    var dataPublicacaoFinal = $('#data_publicacao_final').val();
+    var dataSessaoInicial = $('#data_sessao_inicial').val();
+    var dataSessaoFinal = $('#data_sessao_final').val();
 
-    if (dataInicial != "" || dataFinal != "") {
-        if (dataInicial == "") {
-            mensagem += "<li>Favor, informe a data inicial para efetuar a busca por data.</li>";
+    if (dataPublicacaoInicial != "" || dataPublicacaoFinal != "") {
+        if (dataPublicacaoInicial == "") {
+            mensagem += "<li>Favor, informe a data da publicação inicial para efetuar a busca por data da publicação.</li>";
             $("label[for='data-inicial']").css("color", "red");
         } else {
             $("label[for='data-inicial']").css("color", "#aaa");
         }
 
-        if (dataFinal == "") {
-            mensagem += "<li>Favor, informe a data final para efetuar a busca por data.</li>";
+        if (dataPublicacaoFinal == "") {
+            mensagem += "<li>Favor, informe a data da publicação final para efetuar a busca por data da publicação.</li>";
             $("label[for='data-final']").css("color", "red");
         } else {
             $("label[for='data-final']").css("color", "#aaa");
         }
 
-        if (dataInicial != "" && dataFinal != "") {
-            var inicial = new Date(dataInicial.split('/').reverse().join('/'));
-            var final = new Date(dataFinal.split('/').reverse().join('/'));
+        if (dataPublicacaoInicial != "" && dataPublicacaoFinal != "") {
+            var inicial = new Date(dataPublicacaoInicial.split('/').reverse().join('/'));
+            var final = new Date(dataPublicacaoFinal.split('/').reverse().join('/'));
 
             if (inicial > final) {
-                mensagem += "<li>A data inicial é maior do que a data final.</li>";
+                mensagem += "<li>A data da publicação inicial é maior do que a data da publicação final.</li>";
+                $("label[for='data-inicial']").css("color", "red");
+                $("label[for='data-final']").css("color", "red");
+            } else {
+                $("label[for='data-inicial']").css("color", "#aaa");
+                $("label[for='data-final']").css("color", "#aaa");
+            }
+        }
+    }
+
+    if (dataSessaoInicial != "" || dataSessaoFinal != "") {
+        if (dataSessaoInicial == "") {
+            mensagem += "<li>Favor, informe a data da sessão inicial para efetuar a busca por data da sessão.</li>";
+            $("label[for='data-inicial']").css("color", "red");
+        } else {
+            $("label[for='data-inicial']").css("color", "#aaa");
+        }
+
+        if (dataSessaoFinal == "") {
+            mensagem += "<li>Favor, informe a data da sessão final para efetuar a busca por data da publicação.</li>";
+            $("label[for='data-final']").css("color", "red");
+        } else {
+            $("label[for='data-final']").css("color", "#aaa");
+        }
+
+        if (dataSessaoInicial != "" && dataSessaoFinal != "") {
+            var inicial = new Date(dataSessaoInicial.split('/').reverse().join('/'));
+            var final = new Date(dataSessaoFinal.split('/').reverse().join('/'));
+
+            if (inicial > final) {
+                mensagem += "<li>A data da sessão inicial é maior do que a data da sessão final.</li>";
                 $("label[for='data-inicial']").css("color", "red");
                 $("label[for='data-final']").css("color", "red");
             } else {
