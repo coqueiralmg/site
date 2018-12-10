e66a386895455e19bd14425c42b256580a2b6d22
Fábio Valentim <maitabom@gmail.com>
2018-12-10 15:55:13 -0200

    Criação de finders para atender a nova tela de licitação

--- src/Model/Table/LicitacaoTable.php
+++ src/Model/Table/LicitacaoTable.php
@@ -10,12 +10,27 @@ class LicitacaoTable extends BaseTable
     public function initialize(array $config)
     {
         $this->table('licitacao');
-        $this->primaryKey('id'); 
-        $this->entityClass('Licitacao');       
+        $this->primaryKey('id');
+        $this->entityClass('Licitacao');
     }
 
     public function findAtivo(Query $query, array $options)
     {
         return $query->where(['ativo' => true]);
+    }
+
+    public function findNovo(Query $query, array $options)
+    {
+        return $query->where(['ativo' => true, 'antigo' => false]);
+    }
+
+    public function findAntigo(Query $query, array $options)
+    {
+        return $query->where(['ativo' => true, 'antigo' => true]);
+    }
+
+    public function findDestaque(Query $query, array $options)
+    {
+        return $query->where(['ativo' => true, 'destaque' => true, 'antigo' => false]);
     }
 }
