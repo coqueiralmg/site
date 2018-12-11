12a1994b0121f7aaf9eb9a8e9ba04de13f703b16
Fábio Valentim <maitabom@gmail.com>
2018-12-11 09:56:06 -0200

    Adição de relacionamentos na tabela de licitações e correções de buscas de campos ambíguos nos finders

--- src/Model/Table/LicitacaoTable.php
+++ src/Model/Table/LicitacaoTable.php
@@ -12,25 +12,52 @@ class LicitacaoTable extends BaseTable
         $this->table('licitacao');
         $this->primaryKey('id');
         $this->entityClass('Licitacao');
+
+        $this->belongsTo('Modalidade', [
+            'className' => 'Modalidade',
+            'foreignKey' => 'modalidade',
+            'propertyName' => 'modalidade',
+            'joinType' => 'INNER'
+        ]);
+
+        $this->belongsTo('StatusLicitacao', [
+            'className' => 'StatusLicitacao',
+            'foreignKey' => 'status',
+            'propertyName' => 'status',
+            'joinType' => 'INNER'
+        ]);
+
+        $this->belongsToMany('Assunto', [
+            'joinTable' => 'assunto_licitacao',
+            'foreignKey' => 'licitacao',
+            'targetForeignKey' => 'assunto',
+            'propertyName' => 'assuntos'
+        ]);
+
+        $this->belongsTo('AssuntoLicitacao', [
+            'joinType' => 'INNER',
+            'foreignKey' => 'id',
+            'bindingKey' => 'licitacao',
+        ]);
     }
 
     public function findAtivo(Query $query, array $options)
     {
-        return $query->where(['ativo' => true]);
+        return $query->where(['Licitacao.ativo' => true]);
     }
 
     public function findNovo(Query $query, array $options)
     {
-        return $query->where(['ativo' => true, 'antigo' => false]);
+        return $query->where(['Licitacao.ativo' => true, 'Licitacao.antigo' => false]);
     }
 
     public function findAntigo(Query $query, array $options)
     {
-        return $query->where(['ativo' => true, 'antigo' => true]);
+        return $query->where(['Licitacao.ativo' => true, 'Licitacao.antigo' => true]);
     }
 
     public function findDestaque(Query $query, array $options)
     {
-        return $query->where(['ativo' => true, 'destaque' => true, 'antigo' => false]);
+        return $query->where(['Licitacao.ativo' => true, 'Licitacao.destaque' => true, 'Licitacao.antigo' => false]);
     }
 }
