dff4e34765da023802f6eb3c1ccc515830cd3718
Fábio Valentim <maitabom@gmail.com>
2018-12-11 15:35:41 -0200

    Criação de filtro de pesquisa por modalidade

--- src/Controller/LicitacoesController.php
+++ src/Controller/LicitacoesController.php
@@ -168,6 +168,127 @@ class LicitacoesController extends AppController
         $this->set('opcao_paginacao', $opcao_paginacao);
     }
 
+    public function modalidade(string $codigo)
+    {
+        $conditions = array();
+        $limite_paginacao = Configure::read('Pagination.limit');
+
+        if($this->request->is('get') && count($this->request->query) > 0)
+        {
+            $chave = $this->request->query('chave');
+
+
+            $conditions = $this->montarBusca($chave);
+
+            $data = array();
+
+            $data['chave'] = $chave;
+
+            $this->request->data = $data;
+        }
+
+        $conditions['Licitacao.ativo'] = true;
+        $conditions['Licitacao.antigo'] = false;
+        $conditions['Modalidade.chave'] = $codigo;
+
+        $this->paginate = [
+            'limit' => $limite_paginacao,
+            'conditions' => $conditions,
+            'contain' => ['Modalidade', 'StatusLicitacao'],
+            'order' => [
+                'dataPublicacao' => 'DESC',
+                'dataSessao' => 'DESC'
+            ]
+        ];
+
+        $opcao_paginacao = [
+            'name' => 'licitações',
+            'name_singular' => 'licitação',
+            'predicate' => 'encontradas',
+            'singular' => 'econtrada'
+        ];
+
+        $t_licitacoes = TableRegistry::get('Licitacao');
+        $t_modalidade = TableRegistry::get('Modalidade');
+        $t_assuntos = TableRegistry::get('Assunto');
+        $t_status = TableRegistry::get('StatusLicitacao');
+
+        $licitacoes = $this->paginate($t_licitacoes);
+        $inicial = count($this->request->query) == 0;
+
+        $qtd_total = $t_licitacoes->find('all', [
+            'contain' => ['Modalidade', 'StatusLicitacao'],
+            'conditions' => $conditions])->count();
+
+        $destaques = $populares = $anos = $modalidades = $assuntos = $status = null;
+
+        if($inicial)
+        {
+            $destaques = $t_licitacoes->find('destaque', [
+                'contain' => ['Modalidade', 'StatusLicitacao'],
+                'conditions' => [
+                    'Modalidade.chave' => $codigo
+                ],
+                'order' => [
+                    'dataPublicacao' => 'DESC',
+                    'dataSessao' => 'DESC'
+                ]
+            ]);
+
+            $populares = $t_licitacoes->find('novo', [
+                'limit' => $limite_paginacao,
+                'contain' => ['Modalidade', 'StatusLicitacao'],
+                'conditions' => [
+                    'visualizacoes >' => 0,
+                    'Modalidade.chave' => $codigo
+                ],
+                'order' => [
+                    'visualizacoes' => 'DESC',
+                    'dataPublicacao' => 'DESC',
+                    'dataSessao' => 'DESC'
+                ]
+            ]);
+
+            $anos = $t_licitacoes->find('novo', [
+                        'conditions' => [
+                            'modalidade' => $codigo
+                        ]
+                    ])->select(['ano'])
+                      ->group('ano')
+                      ->order([
+                            'ano' => 'DESC'
+                      ]);
+
+            $assuntos = $t_assuntos->find('all', [
+                'conditions' => [
+                    'tipo' => 'LC'
+                ],
+                'order' => [
+                    'descricao' => 'ASC'
+                ]
+            ]);
+
+            $status = $t_status->find('all', [
+                'order' => [
+                    'ordem' => 'ASC'
+                ]
+            ]);
+        }
+
+        $this->set('title', "Licitações");
+        $this->set('licitacoes', $licitacoes->toArray());
+        $this->set('destaques', $destaques == null ? [] : $destaques->toArray());
+        $this->set('populares', $populares == null ? [] : $populares->toArray());
+        $this->set('assuntos', $assuntos == null ? [] : $assuntos->toArray());
+        $this->set('status', $status == null ? [] : $status->toArray());
+        $this->set('anos', $anos == null ? [] : $anos->toArray());
+        $this->set('qtd_total', $qtd_total);
+        $this->set('inicial', $inicial);
+        $this->set('modalidade', $codigo);
+        $this->set('limit_pagination', $limite_paginacao);
+        $this->set('opcao_paginacao', $opcao_paginacao);
+    }
+
     public function licitacao(string $slug)
     {
         $gate = explode('-', $slug);
