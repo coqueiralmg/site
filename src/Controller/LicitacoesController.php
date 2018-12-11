ab6b60f8b5e38de16ce55f111ea1f4e74fef430f
Fábio Valentim <maitabom@gmail.com>
2018-12-11 14:45:40 -0200

    Criação de action para exibir licitações antigas

--- src/Controller/LicitacoesController.php
+++ src/Controller/LicitacoesController.php
@@ -121,6 +121,53 @@ class LicitacoesController extends AppController
         $this->set('opcao_paginacao', $opcao_paginacao);
     }
 
+    public function antigas()
+    {
+        $conditions = array();
+        $limite_paginacao = Configure::read('Pagination.limit');
+
+        if($this->request->is('get') && count($this->request->query) > 0)
+        {
+            $chave = $this->request->query('chave');
+
+            $conditions['titulo LIKE'] = '%' . $chave . '%';
+
+            $data = array();
+
+            $data['chave'] = $chave;
+
+            $this->request->data = $data;
+        }
+
+        $conditions['ativo'] = true;
+        $conditions['antigo'] = true;
+
+        $this->paginate = [
+            'limit' => $limite_paginacao,
+            'conditions' => $conditions,
+            'order' => [
+                'id' => 'DESC'
+            ]
+        ];
+
+        $opcao_paginacao = [
+            'name' => 'licitações',
+            'name_singular' => 'licitação',
+            'predicate' => 'encontradas',
+            'singular' => 'econtrada'
+        ];
+
+        $t_licitacoes = TableRegistry::get('Licitacao');
+        $licitacoes = $this->paginate($t_licitacoes);
+        $qtd_total = $t_licitacoes->find('all', ['conditions' => $conditions])->count();
+
+        $this->set('title', "Licitações");
+        $this->set('licitacoes', $licitacoes->toArray());
+        $this->set('qtd_total', $qtd_total);
+        $this->set('limit_pagination', $limite_paginacao);
+        $this->set('opcao_paginacao', $opcao_paginacao);
+    }
+
     public function licitacao(string $slug)
     {
         $gate = explode('-', $slug);
