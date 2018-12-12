e041974cde2ab145c8a856cb49d9bf0fff29e164
Fábio Valentim <maitabom@gmail.com>
2018-12-12 15:56:44 -0200

    Criação de validação de licitação

--- src/Model/Entity/Licitacao.php
+++ src/Model/Entity/Licitacao.php
@@ -2,8 +2,9 @@
 
 namespace App\Model\Entity;
 
+use Cake\Core\Configure;
 use Cake\ORM\Entity;
-
+use Cake\ORM\TableRegistry;
 
 class Licitacao extends Entity
 {
@@ -12,7 +13,92 @@ class Licitacao extends Entity
          $titulo = $this->_properties['titulo'];
         $procurar = array(' ', 'ã', 'à', 'á', 'â', 'é', 'ê', 'í', 'ì', 'ó', 'ò', 'õ', 'ô', 'ú', 'ù', 'û', 'ç', ',', '.', '!', '?', ';', '/');
         $substituir = array('_', 'a', 'a', 'a', 'a', 'e', 'e', 'i', 'i', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'c', '', '', '', '', '', '_');
-        
+
         return strtolower(str_replace($procurar, $substituir, $titulo));
+    }
+
+    protected function _getSituacao()
+    {
+        $t_anexos = TableRegistry::get('Anexo');
+
+        $id = $this->_properties['id'];
+        $modalidade = $this->_properties['modalidade']['chave'];
+        $status = $this->_properties['status']['id'];
+        $nomeStatus = $this->_properties['status']['nome'];
+        $dataSessao = (isset($this->_properties['dataSessao']) ? $this->_properties['dataSessao'] : null);
+        $dataFim = (isset($this->_properties['dataFim']) ? $this->_properties['dataFim'] : null);
+
+        $realizacao = Configure::read('Licitacoes.Status.EmRealizacao');
+        $situacao = null;
+
+        if($dataSessao == null || $dataSessao == '')
+        {
+            $dataSessao = '';
+        }
+        else
+        {
+            $dataSessao = $dataSessao->i18nFormat('yyyy-MM-dd HH:mm:ss');
+            $dataSessao = date_create($dataSessao);
+        }
+
+        if($dataFim == null || $dataFim == '')
+        {
+            if($modalidade == 'PP' ||
+               $modalidade == 'TP')
+            {
+                $dataFim = $this->_properties['dataSessao'];
+                $dataFim = $dataFim->modify('+8 hours');
+
+                $dataFim = $dataFim->i18nFormat('yyyy-MM-dd HH:mm:ss');
+                $dataFim = date_create($dataFim);
+            }
+            else
+            {
+                $dataFim = '';
+            }
+        }
+        else
+        {
+            $dataFim = $dataFim->i18nFormat('yyyy-MM-dd HH:mm:ss');
+            $dataFim = date_create($dataFim);
+        }
+
+        $hoje = date_create();
+
+        $qtd_anexos = $t_anexos->find('all', [
+            'conditions' => [
+                'licitacao' => $id,
+                'ativo' => true
+            ]
+        ])->count();
+
+        if($status == $realizacao)
+        {
+            if($qtd_anexos == 0)
+            {
+                $situacao = 'Aguardando o Edital';
+            }
+            else
+            {
+                if($hoje < $dataSessao)
+                {
+                    $situacao = 'Novo';
+                }
+                elseif($hoje >= $dataSessao && $hoje <= $dataFim)
+                {
+                    $situacao = $nomeStatus;
+                }
+                else
+                {
+                    $situacao = 'Em Análise';
+                }
+            }
+        }
+        else
+        {
+            $situacao = $nomeStatus;
+        }
+
+        return $situacao;
     }
 }
