c7043121f932da28c28babf41cbcb73c369f0f56
Fábio Valentim <maitabom@gmail.com>
2018-12-20 16:00:58 -0200

    Criação de ClientController para cadastro interno de ouvidoria

---
+++ admin/webroot/js/controller/ouvidoria.cadastro.js
@@ -0,0 +1,57 @@
+var novoManifestante = false;
+
+$(function () {
+    $("#tipo").change(function () {
+        tratarTipoManifestacao($(this).val());
+    });
+});
+
+function tratarTipoManifestacao(tipo) {
+    if (novoManifestante) {
+        if (tipo == 'IP') {
+            $("#panel_novo_geral").hide();
+            $("#panel_novo_iluminacao").show();
+        } else if (tipo == 'GR') {
+            $("#panel_novo_iluminacao").hide();
+            $("#panel_novo_geral").show();
+        } else {
+            toggleManifestante();
+        }
+    }
+}
+
+function toggleManifestante() {
+    var tipo = $("#tipo").val();
+
+    if (novoManifestante) {
+        $("#panel_novo_iluminacao").hide();
+        $("#panel_novo_geral").hide();
+        $("#panel_escolha").show();
+        novoManifestante = false;
+    } else {
+        if (tipo == 'IP') {
+            $("#panel_escolha").hide();
+            $("#panel_novo_iluminacao").show();
+            novoManifestante = true
+        } else if (tipo == 'GR') {
+            $("#panel_escolha").hide();
+            $("#panel_novo_geral").show();
+            novoManifestante = true
+        } else {
+            tratarTipoManifestacaoCadastro();
+        }
+    }
+}
+
+function tratarTipoManifestacaoCadastro() {
+    swal({
+        title: "Qual é o tipo de ouvidoria?",
+        html: "Antes de cadastrar o novo manifestante, primeiro precisa selecionar o tipo de ouvidoria. Selecione na combo abaixo.",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonClass: 'btn btn-success',
+        cancelButtonClass: 'btn btn-danger',
+        confirmButtonText: 'OK',
+        cancelButtonText: 'Cancelar'
+    });
+}
