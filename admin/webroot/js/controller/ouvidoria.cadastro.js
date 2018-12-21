e542b6d8b26a04eaad225359cb0a930ccac45b97
Fábio Valentim <maitabom@gmail.com>
2018-12-21 13:27:58 -0200

    Implementação da validação de cadastro interno de ouvidoria

--- admin/webroot/js/controller/ouvidoria.cadastro.js
+++ admin/webroot/js/controller/ouvidoria.cadastro.js
@@ -66,6 +66,94 @@ function tratarTipoManifestacaoCadastro() {
     }).then(function (result) {
         $("#tipo").val(result);
         toggleManifestante();
-    });;
+    });
+}
 
+function validar() {
+    var mensagem = "";
+
+    if ($("#assunto").val() === "") {
+        mensagem += "<li> O assunto da manifestação é obrigatório.</li>";
+        $("label[for='assunto']").css("color", "red");
+    } else {
+        $("label[for='assunto']").css("color", "#aaa");
+    }
+
+    if ($("#prioridade").val() === "") {
+        mensagem += "<li> A prioridade da manifestação é obrigatória.</li>";
+        $("label[for='prioridade']").css("color", "red");
+    } else {
+        $("label[for='prioridade']").css("color", "#aaa");
+    }
+
+    if ($("#tipo").val() === "") {
+        mensagem += "<li> O tipo da manifestação é obrigatório.</li>";
+        $("label[for='tipo']").css("color", "red");
+    } else {
+        $("label[for='tipo']").css("color", "#aaa");
+    }
+
+    if ($("#mensagem").val() === "") {
+        mensagem += "<li> É necessário informar o texto da mensagem da manifestação.</li>";
+        $("label[for='mensagem']").css("color", "red");
+    } else {
+        $("label[for='mensagem']").css("color", "#aaa");
+    }
+
+    if (novoManifestante) {
+        var tipo = $("#tipo").val();
+
+        if (tipo == "GR") {
+            if ($("#nome").val() === "") {
+                mensagem += "<li> O nome do manifestante é obrigatório.</li>";
+                $("label[for='nome']").css("color", "red");
+            } else {
+                $("label[for='nome']").css("color", "#aaa");
+            }
+        } else if (tipo == "IP") {
+            if ($("#nome").val() === "") {
+                mensagem += "<li> O nome do manifestante é obrigatório.</li>";
+                $("label[for='nome']").css("color", "red");
+            } else {
+                $("label[for='nome']").css("color", "#aaa");
+            }
+
+            if ($("#endereco").val() === "") {
+                mensagem += "<li> O endereco do manifestante é obrigatório.</li>";
+                $("label[for='endereco']").css("color", "red");
+            } else {
+                $("label[for='endereco']").css("color", "#aaa");
+            }
+
+            if ($("#numero").val() === "") {
+                mensagem += "<li> O numero da residência do manifestante é obrigatório.</li>";
+                $("label[for='numero']").css("color", "red");
+            } else {
+                $("label[for='numero']").css("color", "#aaa");
+            }
+
+            if ($("#bairro").val() === "") {
+                mensagem += "<li> O bairro da residência do manifestante é obrigatório.</li>";
+                $("label[for='bairro']").css("color", "red");
+            } else {
+                $("label[for='bairro']").css("color", "#aaa");
+            }
+        }
+    } else {
+        if ($("#manifestante").val() === "") {
+            mensagem += "<li> É necessário selecionar o nome do manifestante para esta manifestação. Caso não esteja cadastrado, clique em 'Novo Manifestante'.</li>";
+            $("label[for='manifestante']").css("color", "red");
+        } else {
+            $("label[for='manifestante']").css("color", "#aaa");
+        }
+    }
+
+    if (mensagem == "") {
+        removeCache();
+        return true;
+    } else {
+        $("#cadastro_erro").show('shake');
+        $("#details").html("<ol>" + mensagem + "</ol>");
+        return false;
+    }
 }
