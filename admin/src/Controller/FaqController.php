0e9137f8ffc7d71f87b8eb90631de861bf135ad0
Fábio Valentim <maitabom@gmail.com>
2018-12-28 08:38:53 -0200

    Criação do controller de FAQ

---
+++ admin/src/Controller/FaqController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\Table\BaseTable;
+use Cake\Core\Configure;
+use Cake\Datasource\ConnectionManager;
+use Cake\Network\Session;
+use Cake\ORM\TableRegistry;
+use Cake\ORM\Entity;
+use \Exception;
+
+class FaqController extends AppController
+{
+    public function initialize()
+    {
+        parent::initialize();
+    }
+
+    public function index()
+    {
+
+    }
+
+    public function categorias()
+    {
+
+    }
+}
