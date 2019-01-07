6c018489212b347be404c5a07fbdc52ffe4889c2
Fábio Valentim <maitabom@gmail.com>
2019-01-07 15:51:32 -0200

    Criação de actions para página inicial e categorias

--- admin/src/Controller/FaqController.php
+++ admin/src/Controller/FaqController.php
@@ -19,11 +19,16 @@ class FaqController extends AppController
 
     public function index()
     {
-
+        $this->set('title', 'Perguntas e Respostas');
+        $this->set('icon', 'device_unknown');
     }
 
     public function categorias()
     {
+        $t_categoria = TableRegistry::get('Categoria');
+        $limite_paginacao = Configure::read('Pagination.limit');
 
+        $this->set('title', 'Categorias de Perguntas');
+        $this->set('icon', 'device_unknown');
     }
 }
