a7e59074a7bab63a4310a4fe4127c57dd1f84f0e
Fábio Valentim <maitabom@gmail.com>
2019-01-08 14:20:52 -0200

    Criação do ClientController para cadastro de categoria de perguntas

---
+++ admin/webroot/js/controller/faq.categoria.js
@@ -0,0 +1,74 @@
+var modificado = false;
+
+$(function () {
+    $("input, textarea").change(function () {
+        autosave();
+    });
+
+    if (hasCache('categoriaPerguntas', idCategoriaPergunta)) {
+        $("#cadastro_info").show('fade');
+    }
+
+    $(window).bind("beforeunload", function () {
+        if (modificado) {
+            return "É possível que as alterações não estejam salvas.";
+        }
+    });
+});
+
+function restaurar() {
+    var data = getDataCache('categoriaPerguntas', idCategoriaPergunta);
+
+    if (data != null) {
+        $("#nome").val(data.object.nome);
+        $("#descricao").val(data.object.descricao);
+        $("#ativo").prop("checked", data.object.ativo);
+    }
+
+    notificarUsuario("Os dados em cache foram restaurados com sucesso!", "success")
+}
+
+function cancelarRestauracao() {
+    removeCache();
+    notificarUsuario("Você acabou de descartar dados que estão em cache.", "warning")
+}
+
+function autosave() {
+    var data = {
+        id: idCategoriaPergunta,
+        object: {
+            id: idCategoriaPergunta,
+            nome: $("#nome").val(),
+            descricao: $("#descricao").val(),
+            ativo: $("#ativo").is(':checked')
+        }
+    };
+
+    cacheSave('categoriaPerguntas', data);
+    modificado = true;
+}
+
+function removeCache() {
+    removeData('categoriaPerguntas', idCategoriaPergunta);
+    modificado = false;
+}
+
+function validar() {
+    var mensagem = "";
+
+    if ($("#nome").val() === "") {
+        mensagem += "<li> O nome da categoria de perguntas é obrigatório.</li>";
+        $("label[for='nome']").css("color", "red");
+    } else {
+        $("label[for='nome']").css("color", "#aaa");
+    }
+
+    if (mensagem == "") {
+        removeCache();
+        return true;
+    } else {
+        $("#cadastro_erro").show('shake');
+        $("#details").html("<ol>" + mensagem + "</ol>");
+        return false;
+    }
+}
