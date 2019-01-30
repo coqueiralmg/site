b7e4843b312e6ee306ca65e158e03502ab9cb757
Fábio Valentim <maitabom@gmail.com>
2019-01-30 15:16:15 -0200

    Correção de linkagem de pergunta relacionada

--- admin/src/Model/Table/PerguntaRelacionadaTable.php
+++ admin/src/Model/Table/PerguntaRelacionadaTable.php
@@ -7,7 +7,7 @@ class PerguntaRelacionadaTable extends BaseTable
 {
     public function initialize(array $config)
     {
-        $this->table('pergunta');
+        $this->table('perguntas');
         $this->primaryKey('id');
         $this->entityClass('Pergunta');
     }
