926933b7cfeb22d1db342ecb949cbcec22a3668f
Fábio Valentim <maitabom@gmail.com>
2019-02-01 13:49:15 -0200

    Criação do controller para dúvidas

---
+++ src/Controller/DuvidasController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Cake\Core\Configure;
+use Cake\ORM\TableRegistry;
+
+class DuvidasController extends AppController
+{
+    public function index()
+    {
+
+    }
+}
