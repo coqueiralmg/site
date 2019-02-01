cb4a8e7a1acc60daceb4d7c544cd282368527aad
Fábio Valentim <maitabom@gmail.com>
2019-02-01 13:46:59 -0200

    Criação de entidade e tabela de categoria de perguntas

---
+++ src/Model/Table/CategoriaTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model\Table;
+
+class CategoriaTable extends BaseTable
+{
+    public function initialize(array $config)
+    {
+        $this->table('categorias_perguntas');
+        $this->primaryKey('id');
+        $this->entityClass('Categoria');
+    }
+}
