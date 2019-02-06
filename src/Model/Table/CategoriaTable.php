e960862c04acf0308cc90f7c4a42017196bc1322
Fábio Valentim <maitabom@gmail.com>
2019-02-06 10:46:31 -0200

    Criação de relacionamento de categorias entre perguntas, permitindo uma categoria para várias perguntas

--- src/Model/Table/CategoriaTable.php
+++ src/Model/Table/CategoriaTable.php
@@ -2,6 +2,8 @@
 
 namespace App\Model\Table;
 
+use Cake\ORM\Query;
+
 class CategoriaTable extends BaseTable
 {
     public function initialize(array $config)
@@ -9,5 +11,16 @@ class CategoriaTable extends BaseTable
         $this->table('categorias_perguntas');
         $this->primaryKey('id');
         $this->entityClass('Categoria');
+
+        $this->hasMany('Pergunta', [
+            'className' => 'Pergunta',
+            'foreignKey' => 'categoria',
+            'propertyName' => 'perguntas',
+        ]);
+    }
+
+    public function findAtivo(Query $query, array $options)
+    {
+        return $query->where(['ativo' => true]);
     }
 }
