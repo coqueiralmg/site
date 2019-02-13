3b442779ce215b2de135ec0a2dd76c585c874cda
Fábio Valentim <maitabom@gmail.com>
2019-02-13 09:16:17 -0200

    Implementação da listagem de feriados

--- src/Controller/CidadeController.php
+++ src/Controller/CidadeController.php
@@ -21,7 +21,7 @@ class CidadeController extends AppController
            'og:image:width' => '600',
            'og:image:height' => '400'
        ];
-       
+
        $this->set('title', "História de Coqueiral");
        $this->set('socialTags', $socialTags);
     }
@@ -40,7 +40,7 @@ class CidadeController extends AppController
            'og:image:width' => '600',
            'og:image:height' => '400'
        ];
-        
+
         $this->set('title', "O Perfil do Município");
         $this->set('socialTags', $socialTags);
     }
@@ -59,8 +59,78 @@ class CidadeController extends AppController
            'og:image:width' => '600',
            'og:image:height' => '400'
        ];
-        
+
         $this->set('title', "Localização da Cidade");
         $this->set('socialTags', $socialTags);
+    }
+
+    public function feriados()
+    {
+        $socialTags = [
+           'og:locale' => 'pt_BR',
+           'og:type' => 'article',
+           'article:publisher' => 'https://www.facebook.com/prefeituradecoqueiral',
+           'og:title' => 'Feriados em Coqueiral | Prefeitura Municipal de Coqueiral',
+           'og:description' => 'Feriados do Município de Coqueiral - MG.',
+           'og:url' => 'https://coqueiral.mg.gov.br/cidade/feriados',
+           'og:site_name' => 'Prefeitura Municipal de Coqueiral',
+           'og:image' => 'https://coqueiral.mg.gov.br/img/slide/praca.jpg',
+           'og:image:width' => '600',
+           'og:image:height' => '400'
+       ];
+
+
+        $ano = date('Y');
+        $feriados = $this->listarFeriados($ano);
+        $qtd_total = $feriados->count();
+
+        if($qtd_total == 0)
+        {
+            $ano--;
+            $feriados = $this->listarFeriados($ano);
+            $qtd_total = $feriados->count();
+
+            if($qtd_total == 0)
+            {
+                $this->render('vazio');
+                $this->set('title', 'Feriados');
+            }
+            else
+            {
+                $this->set('title', 'Feriados');
+                $this->set('feriados', $feriados);
+                $this->set('ano', $ano);
+                $this->set('socialTags', $socialTags);
+            }
+        }
+        else
+        {
+            $this->set('title', 'Feriados');
+            $this->set('feriados', $feriados);
+            $this->set('ano', $ano);
+            $this->set('socialTags', $socialTags);
+        }
+    }
+
+    private function listarFeriados(int $ano)
+    {
+        $t_feriado = TableRegistry::get('Feriado');
+
+        $condicoes = array();
+
+        $data_inicial = "01/01/$ano";
+        $data_final = "31/12/$ano";
+
+        $condicoes["data >="] = $this->Format->formatDateDB($data_inicial);
+        $condicoes["data <="] = $this->Format->formatDateDB($data_final);
+
+        $feriados = $t_feriado->find('all', [
+            'conditions' => $condicoes,
+            'order' => [
+                'data' => 'ASC'
+            ]
+        ]);
+
+        return $feriados;
     }
 }
